ea059754a637ff63c2b2d7843d2f00a5ac293e3b
Don Drake <ddrake@dreamingmind.com>
2016-01-22 17:39:01 -0800

    establish Package helper

--- src/View/Helper/PackagedHelper.php
+++ src/View/Helper/PackagedHelper.php
@@ -14,11 +14,27 @@ use App\View\Helper\EditionFactoryHelper;
  * 
  * @author dondrake
  */
-class PackagedHelper extends EditionFactoryHelper {
+class PackagedHelper extends EditionedHelper {
 	
 /**
  * This may have special tools that link back to the contained Artworks
  * 
  */	
+	protected function _formatPieceSummary($format, $edition) {
+		$type = strtolower($edition->type);
+		$plural_type = \Cake\Utility\Inflector::pluralize($type);
+		//		"There are up to z pieces for sale";
+		$fluid = $edition->fluid_piece_count;
+		if ($format->hasSalable($fluid)) {
+			$grammar = $format->salable_piece_count($fluid) === 1 ?
+					"There might be 1 $type" :
+					"There are up to {$format->salable_piece_count($fluid)} $plural_type";
+			$salable = sprintf("<p>%s available for sale.</p>\n", $grammar);
+		} else {
+			$salable = "<p class=\"sold_out\">SOLD OUT</p>\n";
+		}
+		echo $salable;
+	}
+	
 	
 }
