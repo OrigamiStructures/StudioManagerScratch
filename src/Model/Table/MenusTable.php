97a5a99b248279c26a19168abc31941f1158cc7b
Jason Tempestini <jason@curlymedia.com>
2016-01-27 13:35:33 -0800

    Updated login / logout menu
    Used root / to force proper url setup

--- src/Model/Table/MenusTable.php
+++ src/Model/Table/MenusTable.php
@@ -42,8 +42,8 @@ class MenusTable extends AppTable{
 			'Go to Dispo' => '/disposition/index',
         ]];
 	protected $account = ['Account' => [
-			'Login' => 'users/users/login',
-			'Logout' => 'users/users/logout',
+			'Login' => '/users/users/login',
+			'Logout' => '/users/users/logout',
 			'Edit My Profile' => '/users/users/profile',
 			'Update Payment Type' => '/users/updatePayment'
 		]];
