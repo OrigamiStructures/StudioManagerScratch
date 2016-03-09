0f6f9180325d7753942d2c54ae206f6e16e8f65c
Don Drake <ddrake@dreamingmind.com>
2016-03-08 22:22:36 -0800

    Comments and other filter improvements

--- src/Lib/Traits/PieceFilterTrait.php
+++ src/Lib/Traits/PieceFilterTrait.php
@@ -19,6 +19,11 @@ use App\Model\Entity\Disposition;
  */
 trait PieceFilterTrait {
 	
+	/**
+	 * Access point for the Pieces table
+	 * 
+	 * @return Table
+	 */
 	public function PiecesTable() {
 		if (!isset($this->Pieces)) {
 			$this->Pieces = \Cake\ORM\TableRegistry::get('Pieces');
@@ -26,62 +31,173 @@ trait PieceFilterTrait {
 		return $this->Pieces;
 	}
 	
+	/**
+	 * Is the piece collected
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
+	 */
 	public function filterCollected($piece, $key = NULL) {
 		return $piece->collected === 1;
 	}
 	
 	/**
+	 * Is the piece un-collected
 	 * 
-	 * @param type $piece
-	 * @param type $key
-	 * @return type
+	 * It may have other dispositions, but it's not collected
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
 	 */
 	public function filterNotCollected($piece, $key = NULL) {
-		return $piece->collected === NULL || $piece->collected === 0;
+		return is_null($piece->collected);
 	}
 	
+	/**
+	 * Is the piece assigned to a format
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
+	 */
 	public function filterAssigned($piece, $key = NULL) {
 		return !is_null($piece->format_id);
 	}
 	
+	/**
+	 * Is the piece unassigned.
+	 * 
+	 * These pieces are still 'virtual', belonging only to the edition
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
+	 */
 	public function filterUnassigned($piece, $key = NULL) {
 		return is_null($piece->format_id);
 	}
 	
+	/**
+	 * Is the peice free of all dispositions?
+	 * 
+	 * This characteristic would make the piece reassignable, even if it 
+	 * is currently assigned to a format
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
+	 */
 	public function filterFluid($piece, $key = NULL) {
 		return $piece->disposition_count === 0;
 	}
 	
 	/**
-	 * Is the piece free of disposition obligations as of $this->_target_date?
+	 * Is the piece free of future disposition obligations as of $this->_target_date?
+	 * 
+	 * This does not account for disposition type. 
+	 * It strictly filters according to date. 
 	 * 
 	 * @param entity $piece
 	 * @param string $key
 	 * @return boolean
 	 * @throws \BadMethodCallException
 	 */
-	public function filterAvailableOn($piece, $key = NULL) {
+	public function filterAvailableOnDate($piece, $key = NULL) {
 		if (!isset($this->target_date)) {
 			throw new \BadMethodCallException('The \'target_date\' property must be set for this filter.');
 		}
 		
+		// this skips out if the piece has attached dispositions at all
 		if ($this->filterFluid($piece)) {
 			return TRUE;
 		}
+		
 		if (!isset($piece->dispositions)) {
-			$proxy = $this->PiecesTable()->get($peice->id, ['contain' => ['Dispositions' => ['conditions' =>['start_date' > $this->target_date]]]]);
-			osd($proxy->toArray());
+			
+			// this works for pieces that don't have contained dispositions
+			$proxy = $this->PiecesTable()->find()
+				->where(['Pieces.id' => $piece->id])
+				->contain(['Dispositions' => function ($q) {
+					return $q->where(['end_date > ' => $this->target_date]);
+				}])
+				->first();
+				
+				return empty($proxy->toArray()['dispositions']);
+
 		} else {
+			
+			// This works for pieces that have contained dispositions
 			$collection = new Collection($piece->dispositions);
 			$future_obligations = $collection->filter([$this, 'filterFutureDispositions']);
 			return $future_obligations->count() == 0;
 		}
 	}
+	
+	/**
+	 * Is the piece free of Unavailable type dispositions?
+	 * 
+	 * A single 'Unavailable' type disposition removes the piece from circulation 
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
+	 * @throws \BadMethodCallException
+	 */
+	public function filterNotUnavailable($piece, $key = NULL) {
+		// this skips out if the piece has attached dispositions at all
+		if ($this->filterFluid($piece)) {
+			return TRUE;
+		}
 		
+		if (!isset($piece->dispositions)) {
+			
+			// this works for pieces that don't have contained dispositions
+			$proxy = $this->PiecesTable()->find()
+				->where(['Pieces.id' => $piece->id])
+				->contain(['Dispositions' => function ($q) {
+					return $q->where(['type' => DISPOSITION_UNAVAILABLE]);
+				}])
+				->first();
+				
+				return empty($proxy->toArray()['dispositions']);
+
+		} else {
+			
+			// This works for pieces that have contained dispositions
+			$collection = new Collection($piece->dispositions);
+			$unavailable = $collection->filter([$this, 'filterUnavailableDispositions']);
+			return $future_obligations->count() == 0;
+		}
+	}
+	
+	/**
+	 * Is the disposition in the future relative to $this->target_date?
+	 * 
+	 * @param object $disposition
+	 * @param string $key
+	 * @return boolean
+	 */
+	public function filterUnavailableDispositions($disposition, $key) {
+		return $disposition->type == DISPOSITION_UNAVAILABLE;
+	}
+	
+	/**
+	 * Is the piece free to be sold on $this->target_date
+	 * 
+	 * If the piece has any dispostion obligations in the future, it will 
+	 * be disqualified from the sale set
+	 * 
+	 * @param entity $piece
+	 * @param string $key
+	 * @return boolean
+	 */
 	public function forSaleOnDate($piece, $key) {
 		if (
 				$this->filterNotCollected($piece) &&
-				$this->filterAvailableOn($piece)
+				$this->filterAvailableOnDate($piece) &&
+				$this->filterNotUnavailable($piece)
 		) {
 			return TRUE;
 		} else {
