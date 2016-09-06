ee9f32474725e95c5edfbfeae09e36f3d25814ee
Don Drake <ddrake@dreamingmind.com>
2016-09-06 09:13:27 -0700

    bake cell for dispositions

---
+++ src/View/Cell/StandingDispositionCell.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\View\Cell;
+
+use Cake\View\Cell;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\Event\EventManager;
+use App\Lib\SystemState;
+use Cake\Cache\Cache;
+
+/**
+ * StandingDisposition cell
+ */
+class StandingDispositionCell extends Cell
+{
+
+    /**
+     * List of valid options that can be passed into this
+     * cell's constructor.
+     *
+     * @var array
+     */
+    protected $_validCellOptions = [];
+	
+	public $SystemState;
+	
+	public $standing_disposition;
+
+	public function __construct(Request $request = null,
+			Response $response = null,
+			EventManager $eventManager = null, array $cellOptions = array()) {
+		parent::__construct($request, $response, $eventManager, $cellOptions);
+		$this->SystemState = $SystemState = $cellOptions['SystemState'];
+		$this->standing_disposition = $standing_disposition = Cache::read($this->SystemState->artistId(), 'dispo');
+		$this->set(compact('standing_disposition', 'SystemState'));
+	}
+
+    /**
+     * Default display method.
+     *
+     * @return void
+     */
+    public function display()
+    {
+		if (is_null($this->request->session()->read('Auth.User')) && is_null($this->SystemState->artistId())) {
+			// if there's no one logged in or no artist id targeted 
+			// we wouldn't show a standing disposition even if one existed
+			// otherwise it was already set in construction if it existed. 
+			$this->set('standing_disposition', FALSE);
+		}
+    }
+	
+}
---
+++ tests/TestCase/View/Cell/StandingDispositionCellTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Test\TestCase\View\Cell;
+
+use App\View\Cell\StandingDispositionCell;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\View\Cell\StandingDispositionCell Test Case
+ */
+class StandingDispositionCellTest extends TestCase
+{
+
+    /**
+     * Request mock
+     *
+     * @var \Cake\Network\Request|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $request;
+
+    /**
+     * Response mock
+     *
+     * @var \Cake\Network\Response|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $response;
+
+    /**
+     * Test subject
+     *
+     * @var \App\View\Cell\StandingDispositionCell
+     */
+    public $StandingDisposition;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->request = $this->getMock('Cake\Network\Request');
+        $this->response = $this->getMock('Cake\Network\Response');
+        $this->StandingDisposition = new StandingDispositionCell($this->request, $this->response);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->StandingDisposition);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test display method
+     *
+     * @return void
+     */
+    public function testDisplay()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
