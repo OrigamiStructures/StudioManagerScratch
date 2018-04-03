b34422524e9cad62fc0080910526f59223114808
Don Drake <ddrake@dreamingmind.com>
2018-04-03 12:00:02 -0700

    Add comments to this mystery class. It’s unused.

--- src/Lib/SState.php
+++ src/Lib/SState.php
@@ -3,6 +3,15 @@ namespace App\Lib;
 
 use Cake\Event\EventListenerInterface;
 
+/**
+ * This appears to be an attemp to simplify access to request and session 
+ * data by making them more global. I'm not sure if there is or was any
+ * legitimate reason for trying to do that. And I'm not sure if this 
+ * strategy could have worked. But one thing is for sure:
+ * 
+ * THIS CLASS IS UNUSED. 
+ */
+
 class SState implements EventListenerInterface {
 
 	public static $request;
