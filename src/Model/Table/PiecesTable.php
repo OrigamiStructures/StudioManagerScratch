0ff274fbf4b8c385ddea71d191b38d979d819bef
Don Drake <ddrake@dreamingmind.com>
2018-10-24 10:17:21 -0700

    class commenting and organization

--- src/Model/Table/PiecesTable.php
+++ src/Model/Table/PiecesTable.php
@@ -70,147 +70,42 @@ class PiecesTable extends AppTable {
         ]);
     }
 
-// https://github.com/OrigamiStructures/StudioManagerScratch/issues/63 
-// and issue 24
-//	public function implementedEvents() {
+	/**
+	 * @todo https://github.com/OrigamiStructures/StudioManagerScratch/issues/63 and issue 24 
+	 */
+	public function implementedEvents() {
 //		return [
 //			'Pieces.fluidFormatPieces' => 'fluidFormatPieces',
 //		];
-//	}
+	}
 
-    /**
-     * Default validation rules.
-     *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
-     */
-    public function validationDefault(Validator $validator) {
-        $validator
-                ->add('id', 'valid', ['rule' => 'numeric'])
-                ->allowEmpty('id', 'create');
+	/**
+	 * Default validation rules.
+	 *
+	 * @param \Cake\Validation\Validator $validator Validator instance.
+	 * @return \Cake\Validation\Validator
+	     */
+	public function validationDefault(Validator $validator) {
+		$validator
+				->add('id', 'valid', ['rule' => 'numeric'])
+				->allowEmpty('id', 'create');
 
-        $validator
-                ->add('number', 'valid', ['rule' => 'numeric'])
-                ->allowEmpty('number');
+		$validator
+				->add('number', 'valid', ['rule' => 'numeric'])
+				->allowEmpty('number');
 
-        $validator
-                ->add('quantity', 'valid', ['rule' => 'numeric'])
-                ->allowEmpty('quantity');
+		$validator
+				->add('quantity', 'valid', ['rule' => 'numeric'])
+				->allowEmpty('quantity');
 
-        $validator
-                ->add('made', 'valid', ['rule' => 'boolean'])
-                ->allowEmpty('made');
+		$validator
+				->add('made', 'valid', ['rule' => 'boolean'])
+				->allowEmpty('made');
 
-        return $validator;
-    }
+		return $validator;
+	}
 
-    /**
-     * After save, clear any effected edition stackQuery cache
-     * 
-     * @param type $event
-     * @param type $entity
-     * @param type $options
-     */
-    public function afterSave($event, $entity, $options) {
-        if (!isset($entity->edition_id)) {
-            $query = $this->find()
-                            ->where(['id' => $entity->id, 'user_id' => $this->SystemState->artistId()])
-                            ->select(['edition_id'])->toArray();
-            $entity->edition_id = $query[0]->edition_id;
-        }
-        $this->clearCache($entity->edition_id);
-        osdLog($entity, 'afterSave on this piece entity');
-    }
-
-    public function assignedFormatPieces($event, $entity, $table) {
-        $pieces = $table->find('all')->where([
-            'edition_id' => $entity->edition_id,
-            'format_id' => $entity->format_id,
-        ]);
-        return $this->assignedPieces($pieces);
-    }
-
-    public function assignedEditionPieces($event, $entity, $table) {
-        $pieces = $table->find('all')->where([
-            'edition_id' => $entity->edition_id,
-            'format_id IS NOT NULL',
-        ]);
-        /**
-         * THIS NEEDS TO BUMP ASSIGNED FORMAT COUNTING
-         * see https://github.com/OrigamiStructures/StudioManagerScratch/issues/24
-         * Currently fixed by EditionStackComponent::_getFormatTriggerPieces()
-         */
-        return $this->assignedPieces($pieces);
-    }
-
-    /**
-     * Callable that calcs CounterCache Pieces that belongTo Formats
-     * 
-     * These counts are actually sums of 'quantity' on the pieces because of 
-     * the way pieces for Open Editions are tracked (avoiding making thousands 
-     * of individual records)
-     * 
-     * @param Event $event
-     * @param Entity $entity
-     * @param Table $table
-     * @return int
-     */
-    public function assignedPieces(Query $query) {
-        $query->select(['id', 'format_id', 'edition_id', 'quantity']);
-        $sum = (new Collection($query->toArray()))->sumOf(
-                function($value) {
-            return $value->quantity;
-        }
-        );
-        return $sum;
-    }
-
-    public function fluidFormatPieces($event, $entity, $table) {
-        $pieces = $table->find('all')->where([
-            'edition_id' => $entity->edition_id,
-            'format_id' => $entity->format_id,
-            'disposition_count' => 0,
-        ]);
-        return $this->fluidPieces($pieces);
-    }
-
-    public function fluidEditionPieces($event, $entity, $table) {
-        $pieces = $table->find('all')->where([
-            'edition_id' => $entity->edition_id,
-            'format_id IS NOT NULL',
-            'disposition_count' => 0,
-        ]);
-        /**
-         * THIS NEEDS TO BUMP FLUID FORMAT PIECES
-         * see https://github.com/OrigamiStructures/StudioManagerScratch/issues/24
-         * Currently fixed by EditionStackComponent::_getFormatTriggerPieces()
-         */
-        return $this->fluidPieces($pieces);
-    }
-
-    /**
-     * Callable for CounterCache Pieces that don't have Dispositions
-     * 
-     * If a piece doesn't have a disposition, it can still be moved between 
-     * any available formats.
-     * 
-     * @param Event $event
-     * @param Entity $entity
-     * @param Table $table
-     * @return integer
-     */
-    public function fluidPieces($query) {
-        $query->select(['id', 'format_id', 'edition_id', 'quantity']);
-        $sum = (new Collection($query->toArray()))->sumOf(
-                function($value) {
-            return $value->quantity;
-        }
-        );
-        return $sum; //die;
-//		}
-    }
-
-    /**
+	/**
      * Returns a rules checker object that will be used for validating
      * application integrity.
      *
@@ -224,6 +119,146 @@ class PiecesTable extends AppTable {
         $rules->add($rules->existsIn(['format_id', 'edition_id'], 'Formats'));
         return $rules;
     }
+
+	/**
+	 * After save, clear any effected edition stackQuery cache
+	 * 
+	 * @param type $event
+	 * @param type $entity
+	 * @param type $options
+	     */
+	public function afterSave($event, $entity, $options) {
+		if (!isset($entity->edition_id)) {
+			$query = $this->find()
+							->where(['id' => $entity->id, 'user_id' => $this->SystemState->artistId()])
+							->select(['edition_id'])->toArray();
+			$entity->edition_id = $query[0]->edition_id;
+		}
+		$this->clearCache($entity->edition_id);
+		osdLog($entity, 'afterSave on this piece entity');
+	}
+
+// <editor-fold defaultstate="collapsed" desc="Callables to support CounterCache Behaviors">
+	/**
+	 * Callable for Format counter-cache behavior
+	 * 
+	 * @param Event $event
+	 * @param Entity $entity
+	 * @param Table $table
+	 * @return int
+	 */
+	public function assignedFormatPieces($event, $entity, $table) {
+		$pieces = $table->find('all')->where([
+			'edition_id' => $entity->edition_id,
+			'format_id' => $entity->format_id,
+		]);
+		return $this->assignedPieces($pieces);
+	}
+
+	/**
+	 * Callable for Edition counter-cache behavior
+	 * 
+	 * @param Event $event
+	 * @param Entity $entity
+	 * @param Table $table
+	 * @return int
+	 */
+	public function assignedEditionPieces($event, $entity, $table) {
+		$pieces = $table->find('all')->where([
+			'edition_id' => $entity->edition_id,
+			'format_id IS NOT NULL',
+		]);
+		/**
+		 * @todo THIS NEEDS TO BUMP ASSIGNED FORMAT COUNTING
+		 * see https://github.com/OrigamiStructures/StudioManagerScratch/issues/24
+		 * Currently fixed by EditionStackComponent::_getFormatTriggerPieces()
+		         */
+		return $this->assignedPieces($pieces);
+	}
+
+	/**
+	 * Callable that calcs CounterCache Pieces that belongTo Formats
+	 * 
+	 * These counts are actually sums of 'quantity' on the pieces because of 
+	 * the way pieces for Open Editions are tracked (avoiding making thousands 
+	 * of individual records)
+	 * 
+	 * @param Event $event
+	 * @param Entity $entity
+	 * @param Table $table
+	 * @return int
+	     */
+	public function assignedPieces(Query $query) {
+		$query->select(['id', 'format_id', 'edition_id', 'quantity']);
+		$sum = (new Collection($query->toArray()))->sumOf(
+				function($value) {
+			return $value->quantity;
+		}
+		);
+		return $sum;
+	}
+
+	/**
+	 * Callable for Format counter-cache behavior
+	 * 
+	 * @param Event $event
+	 * @param Entity $entity
+	 * @param Table $table
+	 * @return int
+	 */
+	public function fluidFormatPieces($event, $entity, $table) {
+		$pieces = $table->find('all')->where([
+			'edition_id' => $entity->edition_id,
+			'format_id' => $entity->format_id,
+			'disposition_count' => 0,
+		]);
+		return $this->fluidPieces($pieces);
+	}
+
+	/**
+	 * Callable for Edition counter-cache behavior
+	 * 
+	 * @param Event $event
+	 * @param Entity $entity
+	 * @param Table $table
+	 * @return int
+	 */
+	public function fluidEditionPieces($event, $entity, $table) {
+		$pieces = $table->find('all')->where([
+			'edition_id' => $entity->edition_id,
+			'format_id IS NOT NULL',
+			'disposition_count' => 0,
+		]);
+		/**
+		 * THIS NEEDS TO BUMP FLUID FORMAT PIECES
+		 * see https://github.com/OrigamiStructures/StudioManagerScratch/issues/24
+		 * Currently fixed by EditionStackComponent::_getFormatTriggerPieces()
+		         */
+		return $this->fluidPieces($pieces);
+	}
+
+	/**
+	 * Callable for CounterCache Pieces that don't have Dispositions
+	 * 
+	 * If a piece doesn't have a disposition, it can still be moved between 
+	 * any available formats.
+	 * 
+	 * @param Event $event
+	 * @param Entity $entity
+	 * @param Table $table
+	 * @return integer
+	     */
+	public function fluidPieces($query) {
+		$query->select(['id', 'format_id', 'edition_id', 'quantity']);
+		$sum = (new Collection($query->toArray()))->sumOf(
+				function($value) {
+			return $value->quantity;
+		}
+		);
+		return $sum; //die;
+//		}
+	}
+// </editor-fold>
 
     /**
      * Make the specified number of new Piece arrays (for TRD use)
@@ -263,129 +298,137 @@ class PiecesTable extends AppTable {
         return $pieces;
     }
 
-    /**
-     * Find pieces that can gain Dispositions in this circmstance
-     * 
-     * I'm not sure if an edition id would ever be sent.
-     * 
-     * When a format_id is sent canDispose() finds:
-     * 		- The already-disposed but still disposable pieces on the format
-     * 		- The fluid pieces in the edition 
-     * 
-     * @param Query $query
-     * @param type $options
-     * @return type
-     */
-    public function findCanDispose(Query $query, $options) {
-        if (!isset($options['format_id'])) {
-            throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
-        }
-        $format_id = $options['format_id'];
-        $edition_id = $this->Formats->find('parentEdition', $options)
-                        ->select(['Editions.id'])
-                        ->toArray()[0]['Editions']->id;
+// <editor-fold defaultstate="collapsed" desc="CUSTOM FINDERS">
+	/**
+	 * Find pieces that can gain Dispositions in this circmstance
+	 * 
+	 * I'm not sure if an edition id would ever be sent.
+	 * 
+	 * When a format_id is sent canDispose() finds:
+	 * 		- The already-disposed but still disposable pieces on the format
+	 * 		- The fluid pieces in the edition 
+	 * 
+	 * @param Query $query
+	 * @param type $options
+	 * @return type
+	     */
+	public function findCanDispose(Query $query, $options) {
+		if (!isset($options['format_id'])) {
+			$msg = "You must pass \$option['edition_id' => (integer)]";
+			throw new \BadMethodCallException($msg);
+		}
+		$format_id = $options['format_id'];
+		$edition_id = $this->Formats->find('parentEdition', $options)
+			->select(['Editions.id'])
+			->toArray()[0]['Editions']->id;
 
-        if (isset($options['edition_id'])) {
-            $conditions['edition_id'] = $options['edition_id'];
-        }
+		if (isset($options['edition_id'])) {
+			$conditions['edition_id'] = $options['edition_id'];
+		}
 
 //		find piece (format_id && format.disposed && piece.free) or (edition.fluid)
-        return $query->where(['Pieces.format_id' => $format_id, 'Pieces.disposition_count >' => 0, /* disposable */])
-                        ->orWhere(['Pieces.edition_id' => $edition_id, 'Pieces.disposition_count' => 0]);
-    }
+		return $query->where([
+			'Pieces.format_id' => $format_id, 
+			'Pieces.disposition_count >' => 0, /* disposable */])
+				->orWhere([
+					'Pieces.edition_id' => $edition_id, 
+					'Pieces.disposition_count' => 0]);
+	}
 
-    /**
-     * Pieces that are not yet assigned to Formats
-     * 
-     * @param Query $query
-     * @param type $options
-     * @return type
-     * @throws \BadMethodCallException
-     */
-    public function findUnassigned(Query $query, $options) {
-        if (!isset($options['edition_id'])) {
-            throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
-        }
-        $query = $query->where([
-            'edition_id' => $options['edition_id'],
-            'format_id IS NULL',
-            'user_id' => $this->SystemState->artistId(),
-        ]);
-        return $query;
-    }
+	/**
+	 * Pieces that are not yet assigned to Formats
+	 * 
+	 * @param Query $query
+	 * @param type $options
+	 * @return type
+	 * @throws \BadMethodCallException
+	     */
+	public function findUnassigned(Query $query, $options) {
+		if (!isset($options['edition_id'])) {
+			throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
+		}
+		$query = $query->where([
+			'edition_id' => $options['edition_id'],
+			'format_id IS NULL',
+			'user_id' => $this->SystemState->artistId(),
+		]);
+		return $query;
+	}
 
-    /**
-     * Pieces that are assigned to formats but not yet disposed
-     * 
-     * @param Query $query
-     * @param type $options
-     * @return type
-     * @throws \BadMethodCallException
-     */
-    public function findFluid(Query $query, $options) {
-        if (!isset($options['edition_id'])) {
-            throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
-        }
-        if (!isset($options['format_id'])) {
-            $option = $options + ['format_id IS NOT NULL',
-                'disposition_count' => 0,
-                'user_id' => $this->SystemState->artistId(),
-            ];
-        } else {
-            $options = $options + [
-                'disposition_count' => 0,
-                'user_id' => $this->SystemState->artistId(),
-            ];
-        }
-        $query->where($options);
-        return $query;
-    }
+	/**
+	 * Pieces that are assigned to formats but not yet disposed
+	 * 
+	 * @param Query $query
+	 * @param type $options
+	 * @return type
+	 * @throws \BadMethodCallException
+	     */
+	public function findFluid(Query $query, $options) {
+		if (!isset($options['edition_id'])) {
+			throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
+		}
+		if (!isset($options['format_id'])) {
+			$option = $options + ['format_id IS NOT NULL',
+				'disposition_count' => 0,
+				'user_id' => $this->SystemState->artistId(),
+			];
+		} else {
+			$options = $options + [
+				'disposition_count' => 0,
+				'user_id' => $this->SystemState->artistId(),
+			];
+		}
+		$query->where($options);
+		return $query;
+	}
 
-    /**
-     * Pieces that are not yet disposed
-     * 
-     * @param Query $query
-     * @param type $options
-     * @return type
-     * @throws \BadMethodCallException
-     */
-    public function findUndisposed(Query $query, $options) {
-        if (!isset($options['edition_id'])) {
-            throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
-        }
-        $query = $query->where([
-            'edition_id' => $options['edition_id'],
-            'disposition_count' => 0,
-            'user_id' => $this->SystemState->artistId(),
-        ]);
-        return $query;
-    }
+	/**
+	 * Pieces that are not yet disposed
+	 * 
+	 * @param Query $query
+	 * @param type $options
+	 * @return type
+	 * @throws \BadMethodCallException
+	     */
+	public function findUndisposed(Query $query, $options) {
+		if (!isset($options['edition_id'])) {
+			throw new \BadMethodCallException("You must pass \$option['edition_id' => (integer)]");
+		}
+		$query = $query->where([
+			'edition_id' => $options['edition_id'],
+			'disposition_count' => 0,
+			'user_id' => $this->SystemState->artistId(),
+		]);
+		return $query;
+	}
 
-    /**
-     * Pieces that are disposed
-     * 
-     * @param Query $query
-     * @param type $options
-     * @return type
-     * @throws \BadMethodCallException
-     */
-    public function findDisposed(Query $query, $options) {
-        if (!isset($options['edition_id'])) {
-            throw new \BadMethodCallException("You must pass \$option['edition_id']");
-        }
-        if (isset($options['format_id'])) {
-            $query->where(['format_id' => $options['format_id'],]);
-        }
-        $query = $query->where([
-            'edition_id' => $options['edition_id'],
-            'disposition_count > 0',
-            'user_id' => $this->SystemState->artistId(),
-        ]);
+	/**
+	 * Pieces that are disposed
+	 * 
+	 * @param Query $query
+	 * @param type $options
+	 * @return type
+	 * @throws \BadMethodCallException
+	     */
+	public function findDisposed(Query $query, $options) {
+		if (!isset($options['edition_id'])) {
+			throw new \BadMethodCallException("You must pass \$option['edition_id']");
+		}
+		if (isset($options['format_id'])) {
+			$query->where(['format_id' => $options['format_id'],]);
+		}
+		$query = $query->where([
+			'edition_id' => $options['edition_id'],
+			'disposition_count > 0',
+			'user_id' => $this->SystemState->artistId(),
+		]);
 
-        return $query;
-    }
+		return $query;
+	}
 
-    /**
+// </editor-fold>
+
+	/**
      * Get the number of the highest numbered piece in an edition
      * 
      * THE ARGUMENTS ARE NOT IDEAL FOR THIS CALL
