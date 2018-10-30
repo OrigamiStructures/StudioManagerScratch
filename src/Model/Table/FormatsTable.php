3eb232de37e4a87161e927b432cdc53f806525b4
Don Drake <ddrake@dreamingmind.com>
2018-10-29 23:48:05 -0700

    Write new finders for Formats

--- src/Model/Table/FormatsTable.php
+++ src/Model/Table/FormatsTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Model\Table;
 
 use App\Model\Entity\Format;
@@ -7,6 +8,7 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 use App\Lib\Traits\EditionStackCache;
+use App\Model\Behavior\IntegerQueryBehavior;
 
 /**
  * Formats Model
@@ -17,10 +19,11 @@ use App\Lib\Traits\EditionStackCache;
  * @property \Cake\ORM\Association\BelongsTo $Subscriptions
  * @property \Cake\ORM\Association\HasMany $Pieces
  */
-class FormatsTable extends AppTable
-{
-	
-	use EditionStackCache;
+class FormatsTable extends AppTable {
+
+    use EditionStackCache;
+
+// <editor-fold defaultstate="collapsed" desc="Core processes">
 
     /**
      * Initialize method
@@ -28,42 +31,48 @@ class FormatsTable extends AppTable
      * @param array $config The configuration for the Table.
      * @return void
      */
-    public function initialize(array $config)
-    {
+    public function initialize(array $config) {
         parent::initialize($config);
+        $this->_initializeProperities();
+        $this->_initializeAssociations();
+        $this->_initializeBehaviors();
+    }
 
-        $this->table('formats');
-        $this->displayField('title');
-        $this->primaryKey('id');
+// <editor-fold defaultstate="collapsed" desc="Initialization methods">
+    protected function _initializeProperities() {
+        $this->setDisplayField('displayTitle');
+    }
 
-        $this->addBehavior('Timestamp');
-		$this->addBehavior('Family');
-		$this->addBehavior('ArtworkStack');
-		$this->addBehavior('CounterCache', [
-			'Editions' => ['format_count'],
-        ]);
-
-//		if (!isset($this->SystemState) || $this->SystemState->is(ARTWORK_SAVE)) {
-//		if ($this->SystemState->is(ARTWORK_SAVE)) {
-			$this->belongsTo('Users',
-					[
-				'foreignKey' => 'user_id',
-			]);
-//		}		
+    protected function _initializeAssociations() {
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',]);
         $this->belongsTo('Images', [
-            'foreignKey' => 'image_id',
-        ]);
+            'foreignKey' => 'image_id',]);
         $this->belongsTo('Editions', [
-            'foreignKey' => 'edition_id',
-        ]);
+            'foreignKey' => 'edition_id',]);
         $this->belongsTo('Subscriptions', [
-            'foreignKey' => 'subscription_id',
-        ]);
+            'foreignKey' => 'subscription_id',]);
         $this->hasMany('Pieces', [
             'foreignKey' => ['format_id', 'edition_id'],
-			'bindingKey' => ['id', 'edition_id'],
+            'bindingKey' => ['id', 'edition_id'],
         ]);
     }
+
+    /**
+     * @todo these behaviors need to be evaluated for use. At 
+     *      least the Family behavior has not been used
+     */
+    protected function _initializeBehaviors() {
+        $this->addBehavior('Timestamp');
+        $this->addBehavior('Family');
+        $this->addBehavior('ArtworkStack');
+        $this->addBehavior('CounterCache', [
+            'Editions' => ['format_count'],
+        ]);
+        $this->addBehavior('IntegerQuery');
+    }
+
+// </editor-fold>
 
     /**
      * Default validation rules.
@@ -71,8 +80,7 @@ class FormatsTable extends AppTable
      * @param \Cake\Validation\Validator $validator Validator instance.
      * @return \Cake\Validation\Validator
      */
-    public function validationDefault(Validator $validator)
-    {
+    public function validationDefault(Validator $validator) {
         $validator
             ->add('id', 'valid', ['rule' => 'numeric'])
             ->allowEmpty('id', 'create');
@@ -105,60 +113,99 @@ class FormatsTable extends AppTable
      * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
      * @return \Cake\ORM\RulesChecker
      */
-    public function buildRules(RulesChecker $rules)
-    {
+    public function buildRules(RulesChecker $rules) {
         $rules->add($rules->existsIn(['user_id'], 'Users'));
         $rules->add($rules->existsIn(['image_id'], 'Images'));
         $rules->add($rules->existsIn(['edition_id'], 'Editions'));
         $rules->add($rules->existsIn(['subscription_id'], 'Subscriptions'));
         return $rules;
     }
-	
-	/**
-	 * After save, clear any effected edition stackQuery cache
-	 * 
-	 * @param type $event
-	 * @param type $entity
-	 * @param type $options
-	 */
-	public function afterSave($event, $entity, $options){
-		if (!isset($entity->edition_id)) {
-			$query = $this->find()
-					->where(['id' => $entity->id, 'user_id' => $this->SystemState->artistId()])
-					->select(['edition_id'])->toArray();
-			$entity->edition_id = $query[0]->edition_id;
-		}
-		$this->clearCache($entity->edition_id);
-		osdLog($entity, 'afterSave on this format entity');
-	}
-	
-	/**
-	 * Get the current select list
-	 * 
-	 * Bug 
-	 * 
-	 * @param Query $query
-	 * @param string $artist_id
-	 * @return query result object
-	 */
-	public function findChoiceList(Query $query, $options) {
-		$this->displayField('display_title');
-		return $query->where(['user_id' => $options['artist_id']])
-				->distinct('description')->find('list');
-	}
-	
-	/**
-	 * Given a format id return the format entity with contained edition entinty (query)
-	 * 
-	 * @param Query $query
-	 * @param type $options
-	 * @throws \BadMethodCallException
-	 */
-	public function findParentEdition(Query $query, $options) {
-		if (!isset($options['format_id'])) {
-			throw new \BadMethodCallException("You must pass \$option['format_id']");
-		}
-		return $query->where(['Formats.id' => $options['format_id']])->contain(['Editions']);
-	}
-	
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Lifecycle Events">
+
+    /**
+     * After save, clear any effected edition stackQuery cache
+     * 
+     * @param type $event
+     * @param type $entity
+     * @param type $options
+     */
+    public function afterSave($event, $entity, $options) {
+        if (!isset($entity->edition_id)) {
+            $query = $this->find()
+                    ->where(['id' => $entity->id, 'user_id' => $this->SystemState->artistId()])
+                    ->select(['edition_id'])->toArray();
+            $entity->edition_id = $query[0]->edition_id;
+        }
+        $this->clearCache($entity->edition_id);
+        osdLog($entity, 'afterSave on this format entity');
+    }
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Choice List engines">
+
+    /**
+     * Get the current select list
+     * 
+     * @todo This named method is set up in many tables but I'm not 
+     *      sure how much it's used in forms. This particular one doesn't 
+     *      appear to be used at all. Also, this one is a bit iffy in terms 
+     *      of the kind of list it would return. Would we really want the 
+     *      choices from such a list? 
+     * 
+     * @param Query $query
+     * @param string $artist_id
+     * @return query result object
+     */
+    public function findChoiceList(Query $query, $options) {
+        $this->setDisplayField('displayTitle');
+        return $query->where(['user_id' => $options['artist_id']])
+                ->distinct('description')->find('list');
+    }
+
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Custom Finders">
+    
+    public function findSoldOut($query, $options = []) {
+        return $query->find('collectedPieceCount', ['value' => ['>', 0]])
+            ->where(['collected_piece_count' => 'assigned_piece_count']);
+    }
+    
+    public function findCollectedPieceCount($query, $options) {
+        return $this->integer($query, 'collected_piece_count', $options['values']);
+    }
+    
+    public function findCollectedPiecePercentage($query, $options) {
+        return $this->integer($query, 
+            'collected_piece_count / assigned_piece_count', 
+            $options['values']);
+    }
+    
+    public function inSubscription($query, $options) {
+        return $this->integer($query, 'subscription_id', $options['values']);
+    }
+    
+    public function findHasFluid($query, $options = []) {
+        return $query->where(['fluid_piece_count >' => 0]);
+    }
+    
+    public function findEmpty($query, $options = []) {
+        return $query->where(['assigned_piece_count' => 0]);
+    }
+    
+    /**
+     * 
+     * @param type $query
+     * @param type $options
+     */
+    public function findSearch($query, $options) {
+        
+    }
+        
+// </editor-fold>
+
 }
