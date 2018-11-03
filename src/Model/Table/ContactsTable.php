a5b9bccd4f8a8791c9fd6eeaed135185188b32aa
Don Drake <ddrake@dreamingmind.com>
2018-11-03 11:14:15 -0700

    Custom finders on Contacts

--- src/Model/Table/ContactsTable.php
+++ src/Model/Table/ContactsTable.php
@@ -8,6 +8,8 @@ use Cake\ORM\Table;
 use Cake\Validation\Validator;
 use Cake\Event\Event;
 use ArrayObject;
+use App\Model\Behavior\IntegerQueryBehavior;
+use App\Model\Behavior\StringQueryBehavior;
 
 /**
  * Contacts Model
@@ -18,20 +20,31 @@ use ArrayObject;
 class ContactsTable extends AppTable
 {
 
+// <editor-fold defaultstate="collapsed" desc="Core">
+
     /**
      * Initialize method
      *
      * @param array $config The configuration for the Table.
      * @return void
      */
-    public function initialize(array $config)
-    {
+    public function initialize(array $config)     {
         parent::initialize($config);
+        $this->_initializeProperties();
+        $this->_initializeBehaviors();
+        $this->_initializeAssociations();
+    }
 
+
+    protected function _initializeProperties() {
         $this->table('contacts');
         $this->displayField('id');
         $this->primaryKey('id');
 
+    }
+
+
+    protected function _initializeBehaviors() {
         $this->addBehavior('Timestamp');
 
         $this->belongsTo('Users', [
@@ -43,14 +56,18 @@ class ContactsTable extends AppTable
         ]);
     }
 
+
+    protected function _initializeAssociations() {
+        
+    }
+
     /**
      * Default validation rules.
      *
      * @param \Cake\Validation\Validator $validator Validator instance.
      * @return \Cake\Validation\Validator
      */
-    public function validationDefault(Validator $validator)
-    {
+    public function validationDefault(Validator $validator)     {
         $validator
             ->add('id', 'valid', ['rule' => 'numeric'])
             ->allowEmpty('id', 'create');
@@ -71,40 +88,97 @@ class ContactsTable extends AppTable
      * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
      * @return \Cake\ORM\RulesChecker
      */
-    public function buildRules(RulesChecker $rules)
-    {
+    public function buildRules(RulesChecker $rules)     {
         $rules->add($rules->existsIn(['user_id'], 'Users'));
         $rules->add($rules->existsIn(['member_id'], 'Members'));
         return $rules;
     }
-        
+
+// </editor-fold>
+
+
+// <editor-fold defaultstate="collapsed" desc="Lifecycle events">
+
     /**
      * Implemented beforeMarshal event
      * 
-     * @param \App\Model\Table\Event $event
-     * @param \App\Model\Table\ArrayObject $data
-     * @param \App\Model\Table\ArrayObject $options
+     * @todo An artist manager may have multiple artists and if they are 
+     *      operating under that mode, this will make a bad record
+     * 
+     * @param Event $event
+     * @param ArrayObject $data
+     * @param ArrayObject $options
      */
-	public function beforeMarshal(Event $event, ArrayObject $data, ArrayObject $options) {
+    public function beforeMarshal(Event $event, ArrayObject $data, ArrayObject $options) {
         $data['user_id'] = $this->SystemState->artistId();
-	}
+    }
 
-	/**
-	 * Make the specified number of new Contact arrays (for TRD use)
-	 * 
-	 * @param integer $count How many contacts are needed
-	 * @param array $default [column => value] to control what data the pieces have
-	 * @param integer $start The index (and number) of the first of the ($count) pieces
-	 * @return array An array of new entity column-value arrays
-	 */
-	public function spawn($count, $default = [], $start = 0) {
-		$columns = $default + [
-			'id' => NULL,
-			'user_id' => $this->SystemState->artistId(),
+// </editor-fold>
+
+    /**
+     * Make the specified number of new Contact arrays (for TRD use)
+     * 
+     * @param integer $count How many contacts are needed
+     * @param array $default [column => value] to control what data the pieces have
+     * @param integer $start The index (and number) of the first of the ($count) pieces
+     * @return array An array of new entity column-value arrays
+     */
+    public function spawn($count, $default = [], $start = 0) {
+        $columns = $default + [
+            'id' => NULL,
+            'user_id' => $this->SystemState->artistId(),
             'label' => 'New'
-		];
-        
+        ];
+
         return array_fill($start, $count, $columns);
-	}
+    }
+    
+// <editor-fold defaultstate="collapsed" desc="Custom Finders">
+    
+    /**
+     * Find contacts by id
+     * 
+     * @param Query $query
+     * @param array $options see IntegerQueryBehavior
+     * @return Query
+     */
+    public function findContacts($query, $options) {
+        return $this->integer($query, 'id', $options['values']);
+    }
+    
+    /**
+     * Find members
+     * 
+     * @param Query $query
+     * @param array $options see IntegerQueryBehavior
+     * @return Query
+     */
+    public function findMembers(Query $query, $options) {
+        return $query->integer($query, 'member_id', $options['values']);
+    }
+    
+    /**
+     * Find the 'kind' label for contacts
+     * 
+     * @param Query $query
+     * @param array $options see IntegerQueryBehavior
+     * @return Query
+     */
+    public function findKind(Query $query, $options) {
+        return $query->string($query, 'label', $options['value']);
+    }
+    
+    /**
+     * Find specific contact address (phone, email, url, etc)
+     * 
+     * @param Query $query
+     * @param array $options see IntegerQueryBehavior
+     * @return Query
+     */
+    public function findDetail(Query $query, $options) {
+        return $query->string($query, 'data', $options['value']);
+    }
+
+// </editor-fold>
 	
 }
