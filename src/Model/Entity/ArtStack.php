eefbfee52f188d2ac54017ba09c7a53a31a16a9b
Don Drake <ddrake@dreamingmind.com>
2018-11-16 21:27:28 -0800

    Baseline, operational version of ArtStack Entity
    This class will probably get some stack methods that are tailored to Artwork stacks. But this works for a start

--- src/Model/Entity/ArtStack.php
+++ src/Model/Entity/ArtStack.php
@@ -17,50 +17,13 @@ class ArtStack extends Entity {
     
     use StackTrait;
     
-    public $artwork;
-    public $editions;
-    public $formats;
-    public $pieces;
-
     /**
      * Fields that can be mass assigned using newEntity() or patchEntity().
      *
      * @var array
      */
     protected $_accessible = [
-        'artwork' => true,
-        'editions' => true,
-        'formats' => true,
-        'pieces' => true,
-        'dispositionsPieces' => true,
-//        '*' => true,
+        '*' => true,
     ];
     
-//    public function __get($property) {
-//        if(in_array($property, array_keys($this->_accessible))) {
-//            osd($property);
-//            return $this->$property;
-//        }
-//        parent::__get($property);
-//    }
-    
-    public function artwork() {
-        return $this->artwork;
-    }
-    public function editions() {
-        return $this->editions;
-    }
-    public function formats() {
-        return $this->formats;
-    }
-    public function pieces() {
-        return $this->pieces;
-    }
-    public function dispositionsPieces() {
-        return $this->dispositionsPieces;
-    }
-//    public function set($property, $value = NULL, array $options = []) {
-//        $this->$property = new Layer($value);
-//    }
-        
 }
