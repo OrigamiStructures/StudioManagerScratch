5aace4e1c57b4e23ee0e08ef31b83f080797e3e0
Don Drake <ddrake@dreamingmind.com>
2018-11-17 12:32:37 -0800

    New stub class to hold and manage multiple Stack type entities

---
+++ src/Model/Lib/StackSet.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Model\Lib;
+
+/**
+ * Description of Stacks
+ *
+ * @author dondrake
+ */
+class StackSet {
+	
+	protected $_stacks = [];
+
+	public function insert($id, $stack) {
+		$this->_stacks[$id] = $stack;
+	}
+	
+	public function all() {
+			return $this->_stacks;
+		}
+
+}
