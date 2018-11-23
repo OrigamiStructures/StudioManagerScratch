4e22990c9ca7b63c3dd9f57578cb59235ae71b82
Don Drake <ddrake@dreamingmind.com>
2018-11-23 08:54:54 -0800

    add comments to StackSet

--- src/Model/Lib/StackSet.php
+++ src/Model/Lib/StackSet.php
@@ -1,8 +1,16 @@
 <?php
 namespace App\Model\Lib;
 
+use App\Model\Entity\StackEntity;
+
 /**
- * Description of Stacks
+ * StackSet
+ * 
+ * This is a collector class which holds sets of Entities that extend StackEntity
+ * 
+ * This class provides access to the stored entities and thier data 
+ * to make it easier to pull out stacks, layers, and merged collections of 
+ * enties from multiple stack.
  *
  * @author dondrake
  */
@@ -10,22 +18,58 @@ class StackSet {
 	
 	protected $_stacks = [];
 
+	/**
+	 * Add another entity to the collection
+	 * 
+	 * @param string $id
+	 * @param StackEntity $stack
+	 */
 	public function insert($id, $stack) {
 		$this->_stacks[$id] = $stack;
 	}
 	
+	/**
+	 * Return all the entities in an array
+	 * 
+	 * @return array
+	 */
 	public function all() {
 		return $this->_stacks;
 	}
 	
+	/**
+	 * Get the IDs of all the primary entities in the stored stack entities
+	 * 
+	 * @return array
+	 */
 	public function members() {
 		return array_keys($this->_stacks);
 	}
 	
+	/**
+	 * Is this the id of one of the primary enities in a stored stack entity
+	 * 
+	 * @param string $id
+	 * @return boolean
+	 */
 	public function isMember($id) {
 		return array_key_exists($id, $this->_stacks);
 	}
 	
+	/**
+	 * Get all layer entities that match
+	 * 
+	 * This is a collection-level method that matches the StackEntity's and Layer's 
+	 * load() methods. These form a pass-through chain, so the allowed 
+	 * arguments here are the same as for StackEntity::load(). 
+	 * 
+	 * Calling load from this level will merge all found results from all 
+	 * the stored StackEntities.
+	 * 
+	 * @param string $layer
+	 * @param mixed $options
+	 * @return array
+	 */
 	public function load($layer, $options = []){
 		$results = [];
 		foreach($this->_stacks as $stack) {
@@ -34,6 +78,13 @@ class StackSet {
 		return $results;
 	}
 	
+	/**
+	 * Return all StackEntities that contain a layer entity with id = $id
+	 * 
+	 * @param string $layer
+	 * @param string $id
+	 * @return array
+	 */
 	public function owner($layer, $id) {
 		$stacks = [];
 		foreach ($this->_stacks as $stack) {
@@ -44,6 +95,18 @@ class StackSet {
 		return $stacks;
 	}
 	
+	/**
+	 * Get all the ids accross all the stored StackEntities for the Layer entities
+	 * 
+	 * This is a collection-level method that matches the StackEntity's and Layer's 
+	 * IDs() methods. These form a pass-through chain. 
+	 * 
+	 * Calling IDs() from this level will merge all found results from all 
+	 * the stored StackEntities.
+	 * 
+	 * @param string $layer
+	 * @return array
+	 */
 	public function IDs($layer) {
 		$ids = [];
 		foreach($this->_stacks as $stack) {
