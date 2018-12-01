ee97bd5cd013f7c9b124bfa9653a9698016fbc60
Don Drake <ddrake@dreamingmind.com>
2018-11-30 21:19:21 -0800

    Bake the StackEntity base class test

---
+++ tests/TestCase/Model/Entity/StackEntityTest.php
@@ -0,0 +1,142 @@
+<?php
+namespace App\Test\TestCase\Model\Entity;
+
+use App\Model\Entity\StackEntity;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Entity\StackEntity Test Case
+ */
+class StackEntityTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Entity\StackEntity
+     */
+    public $StackEntity;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->StackEntity = new StackEntity();
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->StackEntity);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test exists method
+     *
+     * @return void
+     */
+    public function testExists()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test load method
+     *
+     * @return void
+     */
+    public function testLoad()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test count method
+     *
+     * @return void
+     */
+    public function testCount()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test hasNo method
+     *
+     * @return void
+     */
+    public function testHasNo()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test primaryLayer method
+     *
+     * @return void
+     */
+    public function testPrimaryLayer()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test primaryId method
+     *
+     * @return void
+     */
+    public function testPrimaryId()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test primaryEntity method
+     *
+     * @return void
+     */
+    public function testPrimaryEntity()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test distinct method
+     *
+     * @return void
+     */
+    public function testDistinct()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test IDs method
+     *
+     * @return void
+     */
+    public function testIDs()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test linkedTo method
+     *
+     * @return void
+     */
+    public function testLinkedTo()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
