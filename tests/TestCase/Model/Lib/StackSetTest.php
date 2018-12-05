c488cd1611c35aed671f3e18972b9d3f5e87f10c
Don Drake <ddrake@dreamingmind.com>
2018-12-05 14:48:39 -0800

    bake a new test

---
+++ tests/TestCase/Model/Lib/StackSetTest.php
@@ -0,0 +1,119 @@
+<?php
+namespace App\Test\TestCase\Model\Lib;
+
+use App\Model\Lib\StackSet;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Lib\StackSet Test Case
+ */
+class StackSetTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Lib\StackSet
+     */
+    public $StackSet;
+
+    /**
+     * Test insert method
+     *
+     * @return void
+     */
+    public function testInsert()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test all method
+     *
+     * @return void
+     */
+    public function testAll()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test members method
+     *
+     * @return void
+     */
+    public function testMembers()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test element method
+     *
+     * @return void
+     */
+    public function testElement()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test member method
+     *
+     * @return void
+     */
+    public function testMember()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test count method
+     *
+     * @return void
+     */
+    public function testCount()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test isMember method
+     *
+     * @return void
+     */
+    public function testIsMember()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test load method
+     *
+     * @return void
+     */
+    public function testLoad()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test ownerOf method
+     *
+     * @return void
+     */
+    public function testOwnerOf()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test IDs method
+     *
+     * @return void
+     */
+    public function testIDs()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
