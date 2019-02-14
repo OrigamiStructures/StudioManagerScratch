6bd4709da41e540f7acec17722e19b81ba208e98
Don Drake <ddrake@dreamingmind.com>
2019-02-13 23:06:05 -0800

    building out basic output functionality for Member entity
    This gets a lot of the output and value check methods written. no setters yet. and more tools could be added

--- src/Model/Entity/Member.php
+++ src/Model/Entity/Member.php
@@ -3,6 +3,10 @@ namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
 
+define('FIRST_LAST', 'FL');
+define('LAST_FIRST', 'LF');
+define('LABELED', 'LFL');
+
 /**
  * Member Entity.
  *
@@ -34,50 +38,147 @@ class Member extends Entity
         'id' => false,
     ];
     
+	/**
+	 * The assembled `name` property; first/last format
+	 * 
+	 * @return string
+	 */
     public function _getName(){
-        switch ($this->_properties['member_type']) {
+        switch ($this->getType()) {
             case MEMBER_TYPE_PERSON:
-                $name = $this->_properties['first_name'] . ' ' . $this->_properties['last_name'];
+                $name = implode(' ', [
+					$this->getFirstName(), 
+					$this->getLastName()
+				]);
                 break;
-
             default:
-                $name = $this->_properties['first_name'];
+				$name = $this->_institutionName();
                 break;
         }
 		return trim($name);
     }
     
-    public function _getSortName(){
-        switch ($this->_properties['member_type']) {
+	/**
+	 * The assembled `revers name`; last/first format
+	 */
+    public function _getReverseName(){
+        switch ($this->getType()) {
             case MEMBER_TYPE_PERSON:
-                return $this->_properties['last_name'] . ', ' . $this->_properties['first_name'];
+                $name = implode(', ', [
+					$this->getLastName(), 
+					$this->getFirstName(),
+				]);
                 break;
-
             default:
-                return $this->_properties['first_name'];
+				$name = $this->_institutionName();
                 break;
         }
+		return $name;
     }
+	
+	/**
+	 * Returns an institution/group name
+	 * 
+	 * Handles messy usage of the first/last name fields for these 
+	 * names that only need one field
+	 * 
+	 * @todo The Member UX needs to route Institution/Group names to first_name
+	 * 
+	 * @return string
+	 */
+	protected function _institutionName() {
+		if ($this->_properties['first_name'] === $this->_properties['last_name'] ||
+				isset($this->_properties['first_name'])) {
+			$name = $this->_properties['first_name'];
+		} else {
+			$name = $this->_properties['last_name'];
+		}
+		return $name;
+	}
+
+		/**
+	 * Flexible name getter
+	 * 
+	 * Can return 3 variations of concatention
+	 *	FIRST_LAST = first_name last_name
+	 *  LAST_FIRST = last_name, first_name
+	 *  LABELED = memeber_type: first_name last_name
+	 * 
+	 * @param string $format
+	 * @return string
+	 */
+	public function getName($format = FIRST_LAST) {
+		switch ($format) {
+			case FIRST_LAST:
+				return $this->name;
+				break;
+			case LAST_FIRST:
+				return $this->reverseName;
+				break;
+			case LABELED:
+				return "{$this->getType()}: $this->name";
+				break;
+			default:
+				return $this->name;
+				break;
+		}
+	}
+	
+	/**
+	 * Has this Memeber collected artwork
+	 * 
+	 * @return boolean
+	 */
+	public function isCollector() {
+		if (!is_null($this->getCollector()) && $this->getCollector() > 0 ) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+	
+	protected function getCollector() {
+		return $this->collector;
+	}
     
-    /**
-     * Return a properly cased concatenation of the member type and its name.
-     * 
-     * @param string $case either 'title' or 'lower'
-     * @return string the formatted label
-     */
-    public function memberLabel($case) {
-		return $this->name;
-//        switch ($case) {
-//            case "title":
-//                $label = ucwords($this->member_type) . " " . $this->name;
-//                break;
-//            case "lower":
-//                $label = strtolower($this->member_type) . " " . $this->name;
-//                break;
-//            default:
-//                $label = strtolower($this->member_type) . " " . $this->name;
-//                break;
-//        }
-//        return $label;
-    }
+	public function getCollectedCount() {
+		$count = $this->getCollector();
+		if (!is_null($count) && $count > 0 ) {
+			return $count;
+		}
+		return 0;
+	}
+	
+	public function isDispositionParticipant() {
+		if (!is_null($this->getDispositionCount()) && $this->getDispositionCount() > 0 ) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+	
+	public function getDispositionCount() {
+		$count = $this->disposition_count;
+		if (!is_null($count) && $count > 0 ) {
+			return $count;
+		}
+		return 0;
+	}
+	
+	public function isActive() {
+		if($this->active === 1) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+	
+	public function getFirstName() {
+		return $this->first_name;
+	}
+	
+	public function getLastName() {
+		return $this->last_name;
+	}
+	
+	public function getType() {
+		return $this->member_type;
+	}
 }
