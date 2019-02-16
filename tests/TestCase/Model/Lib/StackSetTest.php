d3a75a2e4499add94e8dcc7dddf1ed1d1926f17f
Don Drake <ddrake@dreamingmind.com>
2019-02-15 23:22:18 -0800

    start setting up a new test

--- tests/TestCase/Model/Lib/StackSetTest.php
+++ tests/TestCase/Model/Lib/StackSetTest.php
@@ -11,11 +11,60 @@ class StackSetTest extends TestCase
 {
 
     /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.art_stack_pieces',
+        'app.art_stack_artworks',
+    ];
+
+    /**
      * Test subject
      *
      * @var \App\Model\Lib\StackSet
      */
     public $StackSet;
+
+    public $Pieces;
+    public $Artworks;
+    public $pieceRecords;
+    public $artRecord;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->Pieces = $this->getTableLocator()->get('Pieces');
+        $this->pieceRecords = $this->Pieces->find('all')->toArray();
+        $this->Artworks = $this->getTableLocator()->get('Artworks');
+        $this->artRecord = $this->Artworks->find('all')->toArray();
+        
+        $chunks = array_chunk($this->pieceRecords, 5);
+        $this->fivePieces = $chunks[1];
+
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Pieces);
+        unset($this->pieceRecords);
+        unset($this->Artworks);
+        unset($this->artRecord);
+        unset($this->fivePieces);
+
+        parent::tearDown();
+    }
 
     /**
      * Test insert method
