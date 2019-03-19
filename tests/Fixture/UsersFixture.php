effee4f7d9dc5b06e8e6ca1f4827830c879c541d
Don Drake <ddrake@dreamingmind.com>
2019-03-19 13:14:23 -0700

    fix the users schema and test data

--- tests/Fixture/UsersFixture.php
+++ tests/Fixture/UsersFixture.php
@@ -17,42 +17,80 @@ class UsersFixture extends TestFixture
      */
     // @codingStandardsIgnoreStart
     public $fields = [
-        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'username' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'email' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'password' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'first_name' => ['type' => 'string', 'length' => 50, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'last_name' => ['type' => 'string', 'length' => 50, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'token' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'token_expires' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'api_token' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'activation_date' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'tos_date' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'active' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null],
+        'is_superuser' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null],
+        'role' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => 'user', 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'artist_id' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null],
         'member_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'username' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
-        'password' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
         '_options' => [
             'engine' => 'InnoDB',
-            'collation' => 'latin1_swedish_ci'
+            'collation' => 'utf8_general_ci'
         ],
     ];
     // @codingStandardsIgnoreEnd
 
     /**
-     * Records
+     * Init method
      *
-     * @var array
+     * @return void
      */
-    public $records = [
-        [
-            'id' => '008ab31c-124d-4e15-a4e1-45fccd7becac',
-            'member_id' => 1,
-            'username' => 'dondrake',
-        ],
-        [
-            'id' => '708cfc57-1162-4c5b-9092-42c25da131a9',
-            'member_id' => 2,
-            'username' => 'gaildrake',
-        ],
-        [
-            'id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
-            'member_id' => 3,
-            'username' => 'drakefamily',
-        ],
-    ];
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => '008ab31c-124d-4e15-a4e1-45fccd7becac',
+                'username' => 'dondrake',
+                'email' => 'ddrake@dreamingmind.com',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'first_name' => 'Don',
+                'last_name' => 'Drake',
+                'token' => '45fd1708fed548568686fbea5d717464',
+                'token_expires' => '2019-03-19 20:06:44',
+                'api_token' => 'Lorem ipsum dolor sit amet',
+                'activation_date' => '2019-03-19 20:06:44',
+                'tos_date' => '2019-03-19 20:06:44',
+                'active' => 1,
+                'is_superuser' => 1,
+                'role' => 'Lorem ipsum dolor sit amet',
+                'created' => '2019-03-19 20:06:44',
+                'modified' => '2019-03-19 20:06:44',
+                'artist_id' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'member_id' => 1
+            ],
+			[
+				'id' => '708cfc57-1162-4c5b-9092-42c25da131a9',
+				'member_id' => 2,
+                'password' => 'Lorem ipsum dolor sit amet',
+                'active' => 1,
+                'is_superuser' => 0,
+				'username' => 'gaildrake',
+
+			],
+			[
+				'id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+				'member_id' => 3,
+                'password' => 'Lorem ipsum dolor sit amet',
+                'active' => 0,
+                'is_superuser' => 0,
+				'username' => 'drakefamily',
+			],
+		];
+        parent::init();
+    }
 }
