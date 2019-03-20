d0903ff7b720fbf12f9eeac025d8ff2adbb9b9ff
Don Drake <ddrake@dreamingmind.com>
2019-03-19 18:18:22 -0700

    start preparing Group data for testing

--- tests/Fixture/GroupsFixture.php
+++ tests/Fixture/GroupsFixture.php
@@ -20,8 +20,9 @@ class GroupsFixture extends TestFixture
         'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
-        'user_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'name' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+        'user_id' => ['type' => 'uuid', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'member_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'active' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '1', 'comment' => '', 'precision' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
@@ -33,17 +34,44 @@ class GroupsFixture extends TestFixture
     // @codingStandardsIgnoreEnd
 
     /**
-     * Records
+     * Init method
      *
-     * @var array
+     * @return void
      */
-    public $records = [
-        [
-            'id' => 1,
-            'created' => '2015-12-10 21:29:08',
-            'modified' => '2015-12-10 21:29:08',
-            'user_id' => 1,
-            'name' => 'Lorem ipsum dolor sit amet'
-        ],
-    ];
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'user_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+                'member_id' => 3,
+                'active' => true
+            ],
+            [
+                'id' => 2,
+                'user_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+                'member_id' => 4,
+                'active' => true
+            ],
+            [
+                'id' => 3,
+                'user_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+                'member_id' => 5,
+                'active' => true
+            ],
+            [
+                'id' => 4,
+                'user_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+                'member_id' => 6,
+                'active' => true
+            ],
+            [
+                'id' => 5,
+                'user_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+                'member_id' => 7,
+                'active' => true
+            ],
+        ];
+        parent::init();
+    }
 }
