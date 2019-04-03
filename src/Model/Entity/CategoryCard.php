ca5f63ad919d3795726bd3fc94827645c33ea0e4
Don Drake <ddrake@dreamingmind.com>
2019-04-03 13:21:05 -0700

    build CartegoryCard entity basic functionality

--- src/Model/Entity/CategoryCard.php
+++ src/Model/Entity/CategoryCard.php
@@ -14,4 +14,24 @@ class CategoryCard extends RolodexCard{
 		return TRUE;
 	}
 	
+	public function hasMembers() {
+		return is_a($this->members, '\App\Model\Lib\Layer');
+	}
+	
+	
+	public function memberEntities() {
+		if($this->hasMembers()) {
+			return $this->members->load();
+		}
+		return [];
+	}
+	
+	public function memberIDs() {
+		return $this->valueList('id', $this->memberEntities());
+	}
+	
+	public function members() {
+		return $this->valueList('name', $this->memberEntities());
+	}
+	
 }
