a5d8705cb49a56f372c3ff2703e5dd013eb73996
Don Drake <ddrake@dreamingmind.com>
2019-04-03 18:40:14 -0700

    start building out contactable trait for the rolodex cards

--- src/Model/Traits/ContactableTrait.php
+++ src/Model/Traits/ContactableTrait.php
@@ -10,7 +10,7 @@ namespace App\Model\Traits;
 trait ContactableTrait {
 	
 	public function initializeContactableCard() {
-		$this->layerTables = array_merge($this->layerTables, ['addresses', 'contacts']);
+		$this->layerTables = array_merge($this->layerTables, ['Addresses', 'Contacts']);
 		$this->stackSchema[] = ['name' => 'addresses',	'specs' => ['type' => 'layer']];
 		$this->stackSchema[] = ['name' => 'contacts',	'specs' => ['type' => 'layer']];
 		$this->seedPoints = array_merge($this->seedPoints, [
@@ -22,18 +22,33 @@ trait ContactableTrait {
 	}
 	
 	public function loadFromAddress($ids) {
-		
+		$IDs = $this->Addresses->find('list', ['valueField' => 'member_id'])
+				->where(['id IN' => $ids]);
+		return $this->stacksFromIdentities(array_unique($IDs->toArray()));
 	}
 	
 	public function marshalAddresses($id, $stack) {
+		if ($stack->count('identity')) {
+			$addresses = $this->Addresses->find('all')
+					->where(['member_id' => $id]);
+			$stack->set(['addresses' => $addresses->toArray()]);
+		}
 		return $stack;
 	}
 	
 	public function loadFromContact($ids) {
-		
+		$IDs = $this->Contact->find('list', ['valueField' => 'member_id'])
+				->where(['id IN' => $ids]);
+		return $this->stacksFromIdentities(array_unique($IDs->toArray()));
+
 	}
 	
 	public function marshalContacts($id, $stack) {
+		if ($stack->count('identity')) {
+			$contacts = $this->Contacts->find('all')
+					->where(['member_id' => $id]);
+			$stack->set(['contacts' => $contacts->toArray()]);
+		}		
 		return $stack;
 	}
 	
