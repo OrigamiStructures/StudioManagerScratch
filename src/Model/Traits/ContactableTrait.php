a5965237ced1ffd5f76e6ebf5251bcd2d0e42916
Don Drake <ddrake@dreamingmind.com>
2019-04-03 20:21:13 -0700

    Add basic access methods to contactable trait

--- src/Model/Traits/ContactableTrait.php
+++ src/Model/Traits/ContactableTrait.php
@@ -9,5 +9,43 @@ namespace App\Model\Traits;
  * @author dondrake
  */
 trait ContactableTrait {
+
+	public function hasContacts() {
+		return is_a($this->contacts, '\App\Model\Lib\Layer');
+	}
 	
+	public function contactEntities() {
+		if($this->hasContacts()) {
+			return $this->contacts->load();
+		}
+		return [];
+	}
+	
+	public function memberIDs() {
+		return $this->valueList('id', $this->contactEntities());
+	}
+
+	
+	public function contacts() {
+		return $this->valueList('asString', $this->contactEntities());
+	}
+	
+	public function hasAddresses() {
+		return is_a($this->addresses, '\App\Model\Lib\Layer');
+	}
+	
+	public function addressEntities() {
+		if($this->hasAddresses()) {
+			return $this->addresses->load();
+		}
+		return [];
+	}
+	
+	public function addressIDs() {
+		return $this->valueList('id', $this->addressEntities());
+	}
+
+	public function addresses() {
+		return $this->valueList('asString', $this->addressEntities());
+	}
 }
