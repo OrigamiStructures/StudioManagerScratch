5c65ab63d31be68c35bfe3ab355a7289b021e329
Don Drake <ddrake@dreamingmind.com>
2019-04-25 16:03:50 -0700

    Create a stub entity for ArtistManifest stacks

---
+++ src/Model/Entity/ArtistManifest.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Model\Entity;
+
+use App\Model\Entity\StackEntity;
+
+/**
+ * CakePHP ArtistManifest
+ * @author dondrake
+ */
+class ArtistManifest extends StackEntity {
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	protected $rootName;
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected $rootDisplaySource;
+	
+	
+}
