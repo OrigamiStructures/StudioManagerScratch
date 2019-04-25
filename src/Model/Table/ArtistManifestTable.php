ebc8c58c160ab8f660c7412dce88706ebb49874a
Don Drake <ddrake@dreamingmind.com>
2019-04-25 16:04:51 -0700

    Refine ArtistManifest properties and write distillers

--- src/Model/Table/ArtistManifestTable.php
+++ src/Model/Table/ArtistManifestTable.php
@@ -8,60 +8,70 @@ use App\Model\Table\StacksTable;
  * CakePHP ArtistManifestTable
  * @author dondrake
  */
-class ArtistManifestTable extends StackTable {
+class ArtistManifestsTable extends StackTable {
 	
 	/**
 	 * {@inheritdoc}
 	 */
-	protected $rootName = 'manifest';
+	protected $rootName = 'identity';
 	
 	/**
 	 * {@inheritdoc}
 	 */
-//	protected $rootDisplaySource = 'name';
+	protected $rootDisplaySource = 'name';
 
 	public function initialize(array $config) {
-		$this->setTable('artists');
+		$this->setTable('manifests');
 		parent::initialize($config);
-	    $this->addLayerTable(['Members', 'Permissions', 'DataOwner']);
+	    $this->addLayerTable(['Identity', 'Permissions', 'DataOwner']);
 		$this->addSeedPoint([
 			'data_owner', 
 			'manager',
 			'managers',
 			'manifest', 
 			'manifests',
-			'artist',
-			'artists',
+			'identity',
+			'identities',
 			'permission',
 			'permissions'
 		]);
 		$this->addStackSchema([
+			'identity',
 			'data_owner',
-			'manager',
-			'manifest',
-			'artist',
+			'manifests',
+			'managers',
 			'permissions'
 		]);
 	}
 	
 	public function distillFromManager($ids) {
-		
+		$IDs = $this->Manifests->find('list', ['fieldValue' => 'member_id'])
+				->find('managedBy', ['ids' => $ids]);
+		return array_unique($IDs);
 	}
 	
 	public function distillFromDataOwner($ids) {
-		
+		$IDs = $this->Manifests->find('list', ['fieldValue' => 'member_id'])
+				->find('issuedBy', ['ids' => $ids]);
+		return array_unique($IDs);
 	}
 	
 	public function distillFromManifest($ids) {
-		
+		$IDs = $this->Manifests->find('list', ['fieldValue' => 'member_id'])
+				->where(['id IN' => $ids]);
+		return array_unique($IDs);
 	}
 	
-	public function distillFromArtist($ids) {
-//		$IDs = $this->Manifest->find('manifestFor', ['ids' => [$ids]]);
+	public function distillFromIdentity($ids) {
+		$IDs = $this->Manifests->find('list', ['fieldValue' => 'member_id'])
+				->find('manifestsFor', ['ids' => $ids]);
+		return array_unique($IDs);
 	}
 	
 	public function distillFromPermission($ids) {
-		
+		$manifest_ids = $this->Permissions->find('list', ['fieldValue' => 'manifest_id'])
+				->where(['id IN' => $ids]);
+		return $this->distillFromManifest($manifest_ids);
 	}
 	
 	public function marshalDataOwner($id, $stack) {
