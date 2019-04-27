325bb876179fae04b3145f8bfa7b2a2cc43bf02f
Don Drake <ddrake@dreamingmind.com>
2019-04-26 21:46:19 -0700

    add tests for artist maniffests

--- tests/TestCase/Model/Table/ArtistManifestsTableTest.php
+++ tests/TestCase/Model/Table/ArtistManifestsTableTest.php
@@ -50,7 +50,6 @@ class ArtistManifestsTableTest extends TestCase
 		
 		$this->ArtistManifests = $this->ArtistManifestsTable
 				->find('stacksFor', ['seed' => 'identity', 'ids' => [1]]);
-		debug($this->ArtistManifests);
     }
 
     /**
@@ -73,7 +72,100 @@ class ArtistManifestsTableTest extends TestCase
      */
     public function testInitialize()
     {
-		debug($this->ArtistManifests);
-        $this->markTestIncomplete('Not implemented yet.');
+        
+        $this->assertTrue(
+            is_a(
+                $this->ArtistManifestsTable->Identities, 
+                'App\Model\Table\IdentitiesTable'),
+            
+            'Initialize() did not set up IdentitiesTable (alias for MembersTable).'
+        );
+
+        
+        $this->assertTrue(
+            is_a(
+                $this->ArtistManifestsTable->DataOwners, 
+                'App\Model\Table\DataOwnersTable'),
+            
+            'Initialize() did not set up DataOwnersTable.'
+        );
+
+        
+        $this->assertTrue(
+            is_a(
+                $this->ArtistManifestsTable->Manifests, 
+                'App\Model\Table\ManifestsTable'),
+            
+            'Initialize() did not set up ManifestsTable.'
+        );
+
+        
+        $this->assertTrue(
+            is_a(
+                $this->ArtistManifestsTable->Permissions, 
+                'App\Model\Table\PermissionsTable'),
+            
+            'Initialize() did not set up PermissionsTable.'
+        );
+
+    }
+	
+    public function testArtistManifestsBasicStructure()
+    {
+		
+        $this->assertTrue(
+            is_a($this->ArtistManifests, 'App\Model\Lib\StackSet'),
+            'The found cards did not come packaged in a StackSet.'
+        );
+        
+        $manifest = $this->ArtistManifests->element(0);
+       
+        $this->assertInstanceOf('App\Model\Entity\ArtistManifest', $manifest,
+            'The StackSet does not contain ArtistManifest instances.'
+        );
+        
+        $this->assertInstanceOf('App\Model\Lib\Layer', $manifest->identity,
+            'The cards identity property is not a Layer object');
+        
+        $this->assertInstanceOf(
+				'App\Model\Entity\Identity', 
+				$manifest->identity->element(0),
+            'The cards identity layer does not contain Identity entity objects');
+                
+        $this->assertInstanceOf('App\Model\Lib\Layer', $manifest->data_owner,
+            'The cards data_owner property is not a Layer object');
+        
+        $this->assertInstanceOf(
+				'App\Model\Entity\DataOwner', 
+				$manifest->data_owner->element(0),
+            'The card\'s data_owner does not contain DataOwner entity instances.'
+        );
+                
+        $this->assertInstanceOf('App\Model\Lib\Layer', $manifest->manifests,
+            'The cards manifests property is not a Layer object');
+        
+        $this->assertInstanceOf(
+				'App\Model\Entity\Manifest', 
+				$manifest->manifests->element(0),
+            'The card\'s manifests does not contain Manifest instances.'
+        );
+                
+        $this->assertInstanceOf('App\Model\Lib\Layer', $manifest->managers,
+            'The cards managers property is not a Layer object');
+        
+        $this->assertInstanceOf(
+				'App\Model\Entity\DataOwner', 
+				$manifest->managers->element(0),
+            'The card\'s managers does not contain DataOwner instances.'
+        );
+                
+        $this->assertInstanceOf('App\Model\Lib\Layer', $manifest->permissions,
+            'The cards permissions property is not a Layer object');
+        
+        $this->assertInstanceOf(
+				'App\Model\Entity\Permission', 
+				$manifest->permissions->element(0),
+            'The card\'s permissions does not contain Permission instances.'
+        );
     }
 }
