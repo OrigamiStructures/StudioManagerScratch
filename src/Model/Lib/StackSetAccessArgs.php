18ae6771a8d4ac7e9bca45156f149410bb5ad702
Don Drake <ddrake@dreamingmind.com>
2019-06-21 20:43:19 -0700

    Put the new loadStack() method in play for StackSet
    StackSet now overrides the normal find( ) method to wrap LayerAccessArgs in a StackSetAccessArgs. This adds the loadStack( ) method only for StackSet.
    
    All normal chained methods still work as normal but there is this new return type.
    
    No testing yet

--- src/Model/Lib/StackSetAccessArgs.php
+++ src/Model/Lib/StackSetAccessArgs.php
@@ -11,4 +11,22 @@ use App\Model\Lib\Layer;
 class StackSetAccessArgs extends LayerAccessArgs {
 	
 	
+	
+	public function loadStacks() {
+		
+		$layerName = $this->valueOf('layer');
+		$result = $this->load(LAYERACC_LAYER);
+		$resultIds = $result->IDs();
+		
+		$stacks = [];
+		foreach ($this->data->load() as $stack) {
+			$intersection = array_intersect($stack->$layerName->IDs(), $resultIds);
+			if (count($intersection) > 0) {
+				$stacks[$stack->rootID()] = $stack;
+			}
+		}
+		return $stacks;
+		
+	}
+	
 }
