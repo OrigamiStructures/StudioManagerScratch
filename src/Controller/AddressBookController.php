7e84b12c3bfa569f7465c2297fa4c17381615c21
Jason Tempestini <jason@curlymedia.com>
2019-06-26 21:10:24 -0700

    Add basic pagination

--- src/Controller/AddressBookController.php
+++ src/Controller/AddressBookController.php
@@ -3,16 +3,30 @@
 
 namespace App\Controller;
 
+use Cake\Controller\Component\PaginatorComponent;
+
 
 class AddressBookController extends AppController
 {
+    public $paginate = [
+        'limit' => 5
+    ];
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
+
     public function index()
     {
         $PersonCards = $this->getTableLocator()->get('PersonCards');
-        $ids = $PersonCards->Identities->find('list')
+        $ids = $this->paginate(
+            $PersonCards->Identities->find('list')
             ->order(['last_name'])
-            ->toArray();
-        osd($ids);die;
+        )
+        ->toArray();
+        osd($ids);
         $results = $PersonCards->find('stacksFor',  ['seed' => 'identity', 'ids' => $ids]);
         $this->set('results', $results);
     }
