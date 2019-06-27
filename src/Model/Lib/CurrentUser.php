d5b42d0b28c30dd8ba96a383db44f707872c4f10
Don Drake <ddrake@dreamingmind.com>
2019-06-26 21:51:40 -0700

    New stub class to provide user data to Tables
    Based on Auth->user() data
    
    provided to Tables as currentUser object and as the basis for contextUser which is assumed to allow more finite data sets and act-as features. We’ll see.

---
+++ src/Model/Lib/CurrentUser.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Model\Lib;
+
+use App\Exception\BadClassConfigurationException;
+
+/**
+ * Description of CurrentUser
+ *
+ * @author dondrake
+ */
+class CurrentUser {
+
+	/**
+	 * User record moved in from the Auth system storage repository
+	 *
+	 * [
+	 *	'id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+	 *	'management_token' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+	 *	'username' => 'don',
+	 *	'email' => 'ddrake@dreamingmind.com',
+	 *	'first_name' => 'Don',
+	 *	'last_name' => 'Drake',
+	 *	'activation_date' => object(Cake\I18n\Time) { },
+	 *	'tos_date' => object(Cake\I18n\Time) { },
+	 *	'active' => true,
+	 *	'is_superuser' => false,
+	 *	'role' => 'user',
+	 *	'created' => object(Cake\I18n\Time) { },
+	 *	'modified' => object(Cake\I18n\Time) { },
+	 *	'artist_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+	 *	'member_id' => (int) 1
+	 * ]
+	 * 
+	 * @var array
+	 */
+	protected $data;
+		
+	public function __construct($data, $options = []) {
+		
+		if (is_null($data)) {
+			$message = 'The first request for the CurrentUser object '
+					. 'must include the data to construct it.';
+			throw new BadClassConfigurationException($msg);
+		}
+		
+		$this->data = $data;
+	}
+
+	public function managerId() {
+		return $this->data['management_token'];
+	}
+	
+	public function supervisorId() {
+		return $this->data['management_token'];
+	}
+	
+	public function userId() {
+		return $this->data['id'];
+	}
+	
+//	public function __debugInfo() {
+//		return ['data' => $this->data];
+//	}
+	
+//	public function setUser($data) {
+//		$this->data = $data;
+//	}
+	
+//	public function user() {
+//		return $this->data;
+//	}
+	
+}
