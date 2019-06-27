6b820e7062f639bcacac0f0bf653c5215484962c
Don Drake <ddrake@dreamingmind.com>
2019-06-26 21:55:07 -0700

    Make all Tables use new current/contextUser objects

--- src/Model/Table/AppTable.php
+++ src/Model/Table/AppTable.php
@@ -4,6 +4,7 @@ namespace App\Model\Table;
 use App\Lib\SystemState;
 use Cake\ORM\Table;
 use Cake\Http\Session;
+use App\Model\Lib\CurrentUser;
 
 /**
  * Description of AppTable
@@ -14,17 +15,37 @@ class AppTable extends Table {
 		
 	public $SystemState;
 	
-	protected $session;
-
+	protected $currentUser;
+	
+	protected $contextUser;
 
 	public function __construct(array $config = []){
         if (!empty($config['SystemState'])) {
             $this->SystemState = $config['SystemState'];
 		}
         if (!empty($config['currentUser'])) {
-            $this->currentUser = $config['currentUser'];
+            $this->currentUser = new CurrentUser($config['currentUser']);
+		}
+        if (!empty($config['contextUser'])) {
+            $this->contextUser = new CurrentUser($config['contextUser']);
+		} else {
+            $this->contextUser = new CurrentUser($config['currentUser']);
 		}
 		parent::__construct($config);
-	}	
+	}
+	
+	/**
+	 * Get/make the currentUser object for the table
+	 */
+	public function currentUser() {
+		return $this->currentUser;
+	}
+	
+	/**
+	 * Get/make the currentUser object for the table
+	 */
+	public function contextUser() {
+		return $this->contextUser;
+	}
 	
 }
