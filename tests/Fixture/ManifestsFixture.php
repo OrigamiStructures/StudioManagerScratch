f2f1390c0b34047440855841de7a5d3ea5bc3a11
Don Drake <ddrake@dreamingmind.com>
2019-07-01 18:45:51 -0700

    clean up fixture formatting

--- tests/Fixture/ManifestsFixture.php
+++ tests/Fixture/ManifestsFixture.php
@@ -17,13 +17,66 @@ class ManifestsFixture extends TestFixture
      */
     // @codingStandardsIgnoreStart
     public $fields = [
-        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
-        'member_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => 'id of Member record that is an artist', 'precision' => null, 'autoIncrement' => null],
-        'user_id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => '', 'comment' => 'id of the registered user that controls this \'access/rights\' record', 'precision' => null],
-        'manager_id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => '', 'comment' => 'id of Member record that is the manager', 'precision' => null],
-        'supervisor_id' => ['type' => 'uuid', 'length' => null, 'null' => false, 'default' => '', 'comment' => 'id of Member record that is the supervisor/issuer', 'precision' => null],
-        'publish_manager' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '0', 'comment' => 'whether other managers can see this manager', 'precision' => null],
-        'publish_manager_contact' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '0', 'comment' => 'when published, include primary contact/address visibility', 'precision' => null],
+        'id' => [
+			'type' => 'integer', 
+			'length' => 11, 
+			'unsigned' => true, 
+			'null' => false, 
+			'default' => null, 
+			'comment' => '', 
+			'autoIncrement' => true, 
+			'precision' => null
+		],
+        'member_id' => [
+			'type' => 'integer',
+			'length' => 11,
+			'unsigned' => false,
+			'null' => false,
+			'default' => null,
+			'comment' => 'id of Member record that is an artist',
+			'precision' => null,
+			'autoIncrement' => null
+		],
+        'user_id' => [
+			'type' => 'uuid',
+			'length' => null,
+			'null' => false,
+			'default' => '',
+			'comment' => 'id of the registered user that controls this \'access/rights\' record',
+			'precision' => null
+		],
+        'manager_id' => [
+			'type' => 'uuid',
+			'length' => null,
+			'null' => false,
+			'default' => '',
+			'comment' => 'id of Member record that is the manager',
+			'precision' => null
+		],
+        'supervisor_id' => [
+			'type' => 'uuid',
+			'length' => null,
+			'null' => false,
+			'default' => '',
+			'comment' => 'id of Member record that is the supervisor/issuer',
+			'precision' => null
+		],
+        'publish_manager' => [
+			'type' => 'boolean',
+			'length' => null,
+			'null' => false,
+			'default' => '0',
+			'comment' => 'whether other managers can see this manager',
+			'precision' => null
+		],
+        'publish_manager_contact' => [
+			'type' => 'boolean',
+			'length' => null,
+			'null' => false,
+			'default' => '0',
+			'comment' => 'when published, include primary contact/address visibility',
+			'precision' => null
+		],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
