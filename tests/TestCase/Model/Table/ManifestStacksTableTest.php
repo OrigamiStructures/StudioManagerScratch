5826c6046414967e1b8edf9c2a08b1acdaea496f
Don Drake <ddrake@dreamingmind.com>
2019-07-01 18:46:58 -0700

    fix test. Auth data doesn’t work properly in tests

--- tests/TestCase/Model/Table/ManifestStacksTableTest.php
+++ tests/TestCase/Model/Table/ManifestStacksTableTest.php
@@ -4,6 +4,7 @@ namespace App\Test\TestCase\Model\Table;
 use App\Model\Table\ManifestStacksTable;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use App\Model\Lib\CurrentUser;
 
 /**
  * App\Model\Table\ManifestStacksTable Test Case
@@ -39,8 +40,23 @@ class ManifestStacksTableTest extends TestCase
 		// end person card stuff
 		'app.permissions'
     ];
+	
+	protected $user = 	  [
+	 	'id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+	 	'management_token' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+	 	'username' => 'don',
+	 	'email' => 'ddrake@dreamingmind.com',
+	 	'first_name' => 'Don',
+	 	'last_name' => 'Drake',
+	 	'active' => true,
+	 	'is_superuser' => false,
+	 	'role' => 'user',
+	 	'artist_id' => 'f22f9b46-345f-4c6f-9637-060ceacb21b2',
+	 	'member_id' => 1
+	  ];
 
-    /**
+
+	/**
      * setUp method
      *
      * @return void
@@ -51,6 +67,8 @@ class ManifestStacksTableTest extends TestCase
         $config = TableRegistry::getTableLocator()
 				->exists('ManifestStacks') ? [] : ['className' => ManifestStacksTable::class];
         $this->ManifestStacksTable = TableRegistry::getTableLocator()->get('ManifestStacks', $config);
+		$this->ManifestStacksTable->setCurrentUser($this->user);
+		$this->ManifestStacksTable->setContextUser($this->user);
 		$this->ManifestStacks = $this->ManifestStacksTable
 				->find('stacksFor', ['seed' => 'manifests', 'ids' => [1,2,3,4,5]]);
     }
