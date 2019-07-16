b01b7cd194b4e673af772fed064a8dbf5162a6b4
Don Drake <ddrake@dreamingmind.com>
2019-07-16 14:24:43 -0700

    refactor some test properties to more closely track current naming

--- tests/TestCase/Model/Entity/ArtistManifestStackTest.php
+++ tests/TestCase/Model/Entity/ArtistManifestStackTest.php
@@ -13,9 +13,9 @@ use Cake\ORM\TableRegistry;
 class ArtistManifestStackTest extends TestCase
 {
 
-    public $ManifestStacksTable;
+    public $AtristManifestStacksTable;
 
-	public $ManifestStacks;
+	public $ArtistManifestStacks;
 
 	public $selfManage;
 	
@@ -73,13 +73,6 @@ class ArtistManifestStackTest extends TestCase
 		],
 	];
 
-	/**
-     * Test subject
-     *
-     * @var \App\Model\Entity\ArtistManifestStack
-     */
-    public $ManifestStack;
-
     /**
      * setUp method
      *
@@ -91,17 +84,17 @@ class ArtistManifestStackTest extends TestCase
 		$supervisor = $this->createMock(\App\Model\Entity\PersonCard::class);
         $config = TableRegistry::getTableLocator()
 				->exists('ManifestStacks') ? [] : ['className' => ArtistManifestStacksTable::class];
-        $this->ManifestStacksTable = TableRegistry::getTableLocator()->get('ManifestStacks', $config);
-		$this->ManifestStacksTable->setCurrentUser(new CurrentUser($this->user[0]));
-		$this->ManifestStacksTable->setContextUser(new CurrentUser($this->user[1]));
-		$this->ManifestStacks = $this->ManifestStacksTable
+        $this->AtristManifestStacksTable = TableRegistry::getTableLocator()->get('ManifestStacks', $config);
+		$this->AtristManifestStacksTable->setCurrentUser(new CurrentUser($this->user[0]));
+		$this->AtristManifestStacksTable->setContextUser(new CurrentUser($this->user[1]));
+		$this->ArtistManifestStacks = $this->AtristManifestStacksTable
 				->find('stacksFor', ['seed' => 'manifests', 'ids' => [1,2,3]]);
 		//Supervisor manages self/artist
-		$this->selfManage = $this->ManifestStacks->element(1, LAYERACC_ID);
+		$this->selfManage = $this->ArtistManifestStacks->element(1, LAYERACC_ID);
 		//Supervisor/manager manages another artist
-		$this->otherManage = $this->ManifestStacks->element(2, LAYERACC_ID);
+		$this->otherManage = $this->ArtistManifestStacks->element(2, LAYERACC_ID);
 		//Foreign manager manages Supervisor's artist
-		$this->foreignManage = $this->ManifestStacks->element(3, LAYERACC_ID);
+		$this->foreignManage = $this->ArtistManifestStacks->element(3, LAYERACC_ID);
 //		debug($this->otherManage);
     }
 
@@ -112,8 +105,8 @@ class ArtistManifestStackTest extends TestCase
      */
     public function tearDown()
     {
-        unset($this->ManifestStacks);
-		unset($this->ManifestStacksTable);
+        unset($this->ArtistManifestStacks);
+		unset($this->AtristManifestStacksTable);
 		unset($this->selfManage);
 		unset($this->otherManage);
 		unset($this->foreignManage);
