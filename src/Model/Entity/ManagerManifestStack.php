e11392e77c65de01bc10de58ba9efbbcbee5b0d6
Don Drake <ddrake@dreamingmind.com>
2019-07-16 21:53:37 -0700

    build new ManagerManifestStack classes

---
+++ src/Model/Entity/ManagerManifestStack.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Model\Entity;
+
+use App\Model\Entity\StackEntity;
+
+/**
+ * CakePHP ManifestStackEntity
+ * @author dondrake
+ */
+class ManagerManifestStack extends StackEntity {
+	
+	public function manifest() {
+		return $this->manifest->element(0, LAYERACC_INDEX);
+	}
+	
+	public function supervisorCard() {
+		$id = $this->rootElement()->supervisorId();
+		$card = $this->people
+				->find('identity')
+				->specifyFilter('supervisor_id', $id)
+				->loadStacks();
+		return array_pop($card);
+	}
+	
+	public function managerCard() {
+		$id = $this->rootElement()->managerId();
+		$card = $this->people
+				->find('identity')
+				->specifyFilter('manager_id', $id)
+				->loadStacks();
+		return array_pop($card);
+	}
+	
+    public function selfAssigned()
+    {
+        return $this->rootElement()->selfAssigned();
+	}
+
+    public function accessSummary()
+    {
+        return (!isset($this->permissions) || $this->permissions->count() == 0) 
+		? "Full Access" 
+		: "Limited Access";
+	}
+
+}
