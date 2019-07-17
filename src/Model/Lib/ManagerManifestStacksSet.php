e665d128b87fd6fef4465bf6646b092e1f13a7a9
Don Drake <ddrake@dreamingmind.com>
2019-07-17 08:42:02 -0700

    Fix bad class name

---
+++ src/Model/Lib/ManagerManifestStacksSet.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Model\Lib;
+
+use App\Model\Lib\StackSet;
+
+/**
+ * Description of ManagementManifestSet
+ *
+ * @author dondrake
+ */
+class ManagerManifestStacksSet extends StackSet{
+	
+	public function ownedManagement($supervisor_id) {
+		$owned = $this
+			->find('manifest')
+			->specifyFilter('supervisor_id', $supervisor_id)
+			->loadStacks();
+		return $owned;
+	}
+	
+	public function delegatedManagement($supervisor_id) {
+		$collection = collection($this->load());
+		$delegated = $collection->filter(function($stack) use ($supervisor_id) {
+			return $stack->rootElement()->supervisorId() == $supervisor_id
+					&& !$stack->rootElement()->selfAssigned();
+		});
+		return $delegated->toArray();
+	}
+	
+	public function receivedManagement($supervisor_id) {
+		$received = $this
+			->find('manifest')
+			->specifyFilter('supervisor_id', $supervisor_id, '!=')
+			->loadStacks();
+		return $received;
+	}
+	
+//	public function ownedAritists() {
+//		return [];
+//	}
+//	
+//	public function delegatedArtists() {
+//		return [];
+//	}
+//	
+//	public function recievedArtists() {
+//		return [];
+//	}
+	
+}
