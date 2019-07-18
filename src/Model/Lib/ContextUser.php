7f58cdb44b92e8ddb077721636023a456d3c5869
Don Drake <ddrake@dreamingmind.com>
2019-07-18 13:39:05 -0700

    first draft of ContextUser object (a singleton, persisted in Session)

---
+++ src/Model/Lib/ContextUser.php
@@ -0,0 +1,308 @@
+<?php
+namespace App\Model\Lib;
+
+use Cake\ORM\TableRegistry;
+use Cake\Http\Session;
+use Cake\Http\Exception\BadRequestException;
+
+/**
+ * Description of ContextUser
+ *
+ * @author dondrake
+ */
+class ContextUser {
+		
+	protected $defaultValues = [
+		'artist' => NULL,
+		'manager' => NULL,
+		'supervisor' => NULL
+	];
+
+
+	protected $actorId = [
+		'artist' => NULL,
+		'manager' => NULL,
+		'supervisor' => NULL
+	];
+	
+	protected $actorCard = [
+		'artist' => NULL,
+		'manager' => NULL,
+		'supervisor' => NULL
+	];
+	
+	protected $user;
+	
+	protected $Session = NULL;
+	
+	protected $PersonCardsTable = NULL;
+	
+	static private $instance = NULL;
+	
+	/**
+	 * Construction of the Singleton instance
+	 * 
+	 * The session-persisted version of ContextUser is only and 
+	 * array of data. Those values, if present, are copied into 
+	 * this object and the stored copy of ContextUser (self::$instance) 
+	 * is made to be a reference to this object that holds it 
+	 * (a self-reference in other words)
+	 * 
+	 * @throws BadRequestException
+	 */
+	private function __construct() {
+		$this->Session = new Session();
+		$this->user = $this->Session->read('Auth.User.id');
+		if (is_null($this->user)) {
+			$message = 'The user is not logged in';
+			throw new BadRequestException($message);
+		}
+		$contextUser = $this->Session->read("$this->user.ContextUser");
+		if (!is_null($contextUser)) {
+			foreach(array_keys($contextUser) as $key) {
+				$this->$key = $contextUser[$key];
+			}
+		} else {
+			$this->persist();
+		}
+		self::$instance = $this;
+	}
+	
+	/**
+	 * Access point for this Singleton
+	 * 
+	 * @return type
+	 */
+	static public function instance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new ContextUser();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Has this actor been defined?
+	 * 
+	 * @param string $actor
+	 * @return boolean
+	 */
+	public function has($actor) {
+		$validActor = $this->validateActor($actor);
+		return !is_null($this->actorId[$validActor]);
+	}
+	
+	/**
+	 * Identitfy one of the actors
+	 * 
+	 * This sets the id. The PersonCard will be lazy loaded
+	 * 
+	 * @param string $actor
+	 * @param string|int $id
+	 * @return boolean
+	 */
+	public function set($actor, $id) {
+		$validActor = $this->validateActor($actor);
+		$this->actorId[$validActor] = $id;
+		$this->actorCard[$validActor] = NULL;
+		$this->persist();
+		return TRUE;
+	}
+	
+	/**
+	 * Get the stored actor id or NULL if not set
+	 * 
+	 * @param type $actor
+	 * @return int|string|NULL
+	 */
+	public function getId($actor) {
+		$validActor = $this->validateActor($actor);
+		return $this->actorId[$validActor];
+	}
+	
+	/**
+	 * Get the actor's PersonCard or NULL if not set
+	 * 
+	 * The card is lazy loaded. Only the id is set and stored until 
+	 * this request for further data is made
+	 * 
+	 * @param string $actor
+	 * @return PersonCard|NULL
+	 */
+	public function getCard($actor) {
+		$validActor = $this->validateActor($actor);
+		if ($this->has($validActor)) {
+			$this->lazyLoadCard($validActor);
+		}
+		return $this->actorCard[$validActor];
+	}
+	
+	/**
+	 * Clear on actor or all values
+	 * 
+	 * @param type $actor
+	 * @return void
+	 */
+	public function clear($actor = NULL) {
+		if (is_null($actor)) {
+			$this->Session->delete("$this->user.ContextUser");
+			$this->actorId = $this->defaultValues;
+			$this->actorCard = $this->defaultValues;
+			self::$instance = NULL;
+			self::instance();
+		} else {
+			$validActor = $this->validateActor($actor);
+			$this->actorId[$validActor] = NULL;
+			$this->actorCard[$validActor] = NULL;
+			$this->persist();
+		}
+	}
+	
+	
+// <editor-fold defaultstate="collapsed" desc="PRIVATE METHODS">
+	
+	/**
+	 * Store the current property values in the Session
+	 * 
+	 * The relevant properties are stored as an array
+	 * 
+	 * @throws \App\Model\Lib\Exception
+	 */
+	private function persist() {
+		osd($this);
+		$data = $this->__debugInfo();
+		unset($data['Session'], $data['PersonCardsTable'], $data['instance']);
+		try {
+			$this->Session->write("$this->user.ContextUser", $data);
+		} catch (Exception $exc) {
+			$message = "Couldn't write ContextUser to Session. " . $exc->getMessage();
+			throw $exc;
+		}
+	}
+
+	/**
+	 * Convert the string to lower case and verify it is a known value
+	 * 
+	 * @param type $actor
+	 * @return string The validated string in lower case
+	 */
+	private function validateActor($actor) {
+		$validActor = strtolower($actor);
+		if (!in_array($actor, array_keys($this->actorId))) {
+			$this->badActor($actor);
+		}
+		return $validActor;
+	}
+
+	/**
+	 * Common Exception point when an invalid actor is referenced
+	 * 
+	 * @param string $actor
+	 * @throws Exception
+	 */
+	private function badActor($actor) {
+		$message = "$actor is not a valid actor focus point. "
+				. "Choose actor, manager, or supervisor";
+		throw new Exception($message);
+	}
+
+	/**
+	 * get the PersonCard or load it and return it
+	 * 
+	 * You won't get here unless the corresponding actor id is set
+	 * 
+	 * @param string $validActor
+	 * @return PersonCard
+	 */
+	private function lazyLoadCard($validActor) {
+		$Card = $this->actorCard[$validActor];
+		if (is_null($Card)) {
+			$method = "{$validActor}Card";
+			$Card = $this->$method();
+			$this->persist();
+		}
+		return $Card;
+	}
+
+	/**
+	 * get or create a Table instance
+	 * 
+	 * @return PersoCardsTable
+	 */
+	private function PersonCardsTable() {
+		if (is_null($this->PersonCardsTable)) {
+			$this->PersonCardsTable = 
+				 TableRegistry::getTableLocator()->get('PersonCardsTable');
+			osd(get_class($this->PersonCardsTable));die;
+		}
+		return $this->PersonCardsTable;
+	}
+
+	/**
+	 * Load and store the artist Person Card
+	 */
+	private function artistCard() {
+		$set = $this->PersonCardsTable()
+				->find('stacksFor',
+				[
+			'seed' => 'id',
+
+			'ids' => [$this->actorId['artist']]
+		]);
+		$this->actorCard['artist'] = $set->shift();
+	}
+
+	/**
+	 * Load and store the artist Supervisor Card
+	 */
+	private function supervisorCard() {
+		$set = $this
+				->PersonCardsTable()
+				->find('stacksFor',
+				[
+			'seed' => 'supervisor',
+
+			'ids' => [$this->actorId['supervisor']]
+		]);
+		$this->actorCard['supervisor'] = $set->shift();
+	}
+
+	/**
+	 * Load and store the artist Manager Card
+	 */
+	private function managerCard() {
+		$set = $this
+				->PersonCardsTable()
+				->find('stacksFor',
+				[
+			'seed' => 'manager',
+
+			'ids' => [$this->actorId['manager']]
+		]);
+		$this->actorCard['manager'] = $set->shift();
+	}
+
+	/**
+	 * Also used by persist( ) to make the session storage array
+	 * 
+	 * @return array
+	 */
+	public function __debugInfo() {
+		return [
+			'user' => $this->user,
+			'actorId' => $this->actorId,
+			'actorCard' => $this->actorCard,
+			'Session' => is_object($this->Session) ? 'Session object' : 'Not set',
+			'PersonCardsTable' =>
+
+			is_object($this->PersonCardsTable) 
+			? 'PersonCardsTable object' 
+			: 'Not set',
+			'instance' =>
+
+			is_null(self::$instance) ? 'instance is clear' : 'instance is populated'
+		];
+	}
+
+	// </editor-fold>
+	
+}
