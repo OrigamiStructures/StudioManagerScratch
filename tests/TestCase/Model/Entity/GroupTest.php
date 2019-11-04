65e0310c4ae0c1bf8ddcc067350d9213b1de3f47
dreamingmind <ddrake@dreamingmind.com>
2019-11-03 20:15:35 -0800

    just reformatting

---
+++ tests/TestCase/Model/Entity/GroupTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Test\TestCase\Model\Entity;
+
+use App\Model\Entity\Group;
+
+class GroupTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test_getDisplayTitle()
+    {
+
+    }
+}
