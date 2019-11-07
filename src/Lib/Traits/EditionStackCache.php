8c9c33e6deb1747b5e7abbb3d8632a6da8499c1c
dreamingmind <ddrake@dreamingmind.com>
2019-11-07 15:01:52 -0800

    formatting

--- src/Lib/Traits/EditionStackCache.php
+++ src/Lib/Traits/EditionStackCache.php
@@ -9,40 +9,40 @@ use Cake\Cache\Cache;
  * @author dondrake
  */
 trait EditionStackCache {
-	
+
 	protected $configuration = 'editionStack';
 
 	/**
 	 * Delete an edition type stackQuery
-	 * 
+	 *
 	 * @param string $id
 	 */
 	public function clearCache($id) {
 		return Cache::delete($this->cache_key($id), $this->configuration);
 	}
-	
+
 	/**
 	 * Read an edition type stackQuery
-	 * 
+	 *
 	 * @param string $id
 	 */
 	public function readCache($id) {
 		return Cache::read($this->cache_key($id), $this->configuration);
 	}
-	
+
 	/**
 	 * Write an edition type stackQuery
-	 * 
+	 *
 	 * @param string $id
 	 * @param mixed $data
 	 */
 	public function writeCache($id, $data) {
 		return Cache::write($this->cache_key($id), $data, $this->configuration);
 	}
-	
+
 	/**
 	 * Create the edition type stackQuery cache key
-	 * 
+	 *
 	 * @param string $id
 	 * @return string
 	 */
