1394f458dcdf81b4d3b847aa2c5c47b49d4c92a1
dreamingmind <ddrake@dreamingmind.com>
2019-11-14 21:17:14 -0800

    formatting

--- src/Model/Lib/StackSet.php
+++ src/Model/Lib/StackSet.php
@@ -2,32 +2,32 @@
 namespace App\Model\Lib;
 
 use App\Model\Entity\StackEntity;
-use App\Interfaces\LayerAccessInterface;
+use App\Interfaces\xxxLayerAccessInterface;
 use App\Model\Traits\LayerAccessTrait;
 use App\Model\Lib\StackSetAccessArgs;
 
 /**
  * StackSet
- * 
+ *
  * This is a collector class which holds sets of Entities that extend StackEntity
- * 
- * This class provides access to the stored entities and their data 
- * to make it easier to pull out stacks, layers, and merged collections of 
+ *
+ * This class provides access to the stored entities and their data
+ * to make it easier to pull out stacks, layers, and merged collections of
  * entities from multiple stack.
  *
  * @author dondrake
  */
-class StackSet implements LayerAccessInterface {
-	
+class StackSet implements xxxLayerAccessInterface {
+
 	use LayerAccessTrait;
-	
+
 	protected $_data = [];
-	
+
 	protected $_stackName;
 
 	/**
 	 * Add another entity to the collection
-	 * 
+	 *
 	 * @param string $id
 	 * @param StackEntity $stack
 	 */
@@ -37,14 +37,14 @@ class StackSet implements LayerAccessInterface {
 			$this->_stackName = $stack->rootLayerName();
 		}
 	}
-	
+
 	public function shift() {
 		return $this->element(0, LAYERACC_INDEX);
 	}
-	
+
 	/**
 	 * Return all the entities in an array
-	 * 
+	 *
 	 * @return array
 	 */
 	public function all() {
@@ -68,32 +68,32 @@ class StackSet implements LayerAccessInterface {
 		}
         return $args;
     }
-		
+
 	/**
 	 * Perform data load from StackSet context
-	 * 
+	 *
 	 * No args will get the id-indexed array of stack entities
 	 * No layer specified will get the paginated chunck of the stack entity array
-	 * Once a layer is specified, load will deligate to each stack entity 
-	 * in turn. Filtering and pagination will be done, and the accumulated 
+	 * Once a layer is specified, load will deligate to each stack entity
+	 * in turn. Filtering and pagination will be done, and the accumulated
 	 * result will be returned
-	 * 
+	 *
 	 * @param mixed $argObj
 	 * @return array
 	 */
 	public function load($argObj = null) {
-		
+
 		if (is_null($argObj)) {
 			return $this->_data;
 		}
-		
+
 		if (is_string($argObj)) {
 			$argObj = (new LayerAccessArgs())
 					->setLayer($argObj);
 		}
-		
+
 		$this->verifyInstanceArgObj($argObj);
-		
+
 		if (!$argObj->hasLayer()) {
 			return $this->paginate($this->_data, $argObj);
 		} else {
@@ -104,33 +104,33 @@ class StackSet implements LayerAccessInterface {
 //				debug($result);
 			}
 		}
-		
+
 		return $result;
-		
+
 	}
-	
+
     /**
      * Get the count of stored Stack objects
-     * 
+     *
      * @return integer
      */
     public function count() {
         return count($this->_data);
     }
-	
+
 	/**
 	 * Is this the id of one of the primary enities in a stored stack entity
-	 * 
+	 *
 	 * @param string $id
 	 * @return boolean
 	 */
 	public function isMember($id) {
 		return array_key_exists($id, $this->_data);
 	}
-	
+
 	/**
 	 * Return all StackEntities that contain a layer entity with id = $id
-	 * 
+	 *
 	 * @param string $layer
 	 * @param string $id
 	 * @return array
@@ -148,21 +148,21 @@ class StackSet implements LayerAccessInterface {
         }
 		return $stacks;
 	}
-	
+
 // <editor-fold defaultstate="collapsed" desc="LAYER ACCESS INTERFACE REALIZATION">
-	
+
 	/**
 	 * Get all the ids accross all the stored StackEntities or the Layer entities
-	 * 
-	 * This is a collection-level method that matches the StackEntity's and Layer's 
-	 * IDs() methods. These form a pass-through chain. 
-	 * 
-	 * Calling IDs() from this level will insure unique results if 
-	 * Layer IDs are pulled. 
-	 * 
+	 *
+	 * This is a collection-level method that matches the StackEntity's and Layer's
+	 * IDs() methods. These form a pass-through chain.
+	 *
+	 * Calling IDs() from this level will insure unique results if
+	 * Layer IDs are pulled.
+	 *
 	 * StackEntity IDs will be from the primary entity propery and will
 	 * be unique becuase the set structure insures it.
-	 * 
+	 *
 	 * @param string $layer
 	 * @return array
 	 */
@@ -178,7 +178,7 @@ class StackSet implements LayerAccessInterface {
 	}
 
 	public function keyedList(LayerAccessArgs $argObj) {
-		
+
 	}
 
 	public function filter($property, $value) {
@@ -195,5 +195,5 @@ class StackSet implements LayerAccessInterface {
 	}
 
 // </editor-fold>
-	
+
 }
