c68b9ecaf9841d9e8c9cc88addc4511d10986970
dreamingmind <ddrake@dreamingmind.com>
2019-11-20 20:48:12 -0800

    formatting

--- src/Model/Table/ArtistCardsTable.php
+++ src/Model/Table/ArtistCardsTable.php
@@ -5,27 +5,27 @@ use App\Model\Table\PersonCardsTable;
 
 /**
  * CakePHP ArtistCardsTable
- * 
+ *
  * @todo When is this enstantiated? Note below:
- * There is more speculation below, but it seems obvious at this 
- * moment that these are made when a manager needs to work with 
- * or for their artist. Or when a registered user needs to 
+ * There is more speculation below, but it seems obvious at this
+ * moment that these are made when a manager needs to work with
+ * or for their artist. Or when a registered user needs to
  * act as themselves as an artist.
- * 
- * The name of this card implies it will only be made if the Identity 
- * also appears as the member_id of an Artist record. There is no code 
+ *
+ * The name of this card implies it will only be made if the Identity
+ * also appears as the member_id of an Artist record. There is no code
  * in the distillers or marshaller to guarantee this fact.
- * 
- * If the RolodexCardsTable is a factory it is possible that it will 
- * take steps during after distillation to enhance the root ID set to 
- * make full determination of the table types required for each stack. 
- * I don't really understand the use patterns enough to know how to 
+ *
+ * If the RolodexCardsTable is a factory it is possible that it will
+ * take steps during after distillation to enhance the root ID set to
+ * make full determination of the table types required for each stack.
+ * I don't really understand the use patterns enough to know how to
  * proceed at this point.
- * 
+ *
  * @author dondrake
  */
 class ArtistCardsTable extends PersonCardsTable {
-		
+
 	public function initialize(array $config) {
 		parent::initialize($config);
 	    $this->addLayerTable(['Manifests', 'Artworks']);
@@ -36,23 +36,23 @@ class ArtistCardsTable extends PersonCardsTable {
 					'manager', 'managers'
 				]);
 	}
-	
+
 	protected function distillFromManifest($ids) {
 		$query = $this->Manifests->find('list', ['valueField' => 'member_id'])
 				->where(['id IN' => $ids]);
 		return $this->distillFromIdentity($query->toArray());
 //		return array_unique($IDs);
 	}
-	
+
 	protected function distillFromArtwork($ids) {
 		$query = $this->Artworks->find('list', ['valueField' => 'member_id'])
 				->where(['id IN' => $ids]);
 		return $this->distillFromIdentity($query->toArray());
 	}
-	
+
 	/**
-	 * 
-	 * 
+	 *
+	 *
 	 * @param array $ids
 	 * @return array
 	 */
@@ -61,32 +61,32 @@ class ArtistCardsTable extends PersonCardsTable {
 				->find('managedBy', ['ids' => $ids]);
 		return $this->distillFromIdentity($query->toArray());
 	}
-	
+
 	protected function marshalArtworks($ids, $stack) {
 		if ($stack->count('identity')) {
 			$artworks = $this->Artworks->find('all')
 					->where(['member_id' => $stack->rootId()]);
 			$stack->set(['artworks' => $artworks->toArray()]);
-		}		
+		}
 		return $stack;
 	}
-	
+
 	protected function marshalManifest($id, $stack) {
 		if ($stack->count('identity')) {
 			$image = $this->Manifests->find('all')
 					->where(['member_id' => $stack->rootId()]);
 			$stack->set(['manifest' => $image->toArray()]);
-		}		
+		}
 		return $stack;
 	}
-	
+
 	/**
 	 * Get the permitted Manager hook data
-	 * 
+	 *
 	 * @todo This is the point we should honor Artist permission settings
-	 * 
+	 *
 	 * @todo make a map_reducer?
-	 * 
+	 *
 	 * @param string $id
 	 * @param StackEntity $stack
 	 * @return StackEntity
@@ -96,7 +96,7 @@ class ArtistCardsTable extends PersonCardsTable {
 			$managerIds = $stack->manifest->valueList('manager_id');
 			$dataOwner = $this->associations()->get('DataOwners')
 				->find(
-					'hook', 
+					'hook',
 					['contain' => [
 						'Members' =>[
 							'fields' => [
@@ -108,10 +108,10 @@ class ArtistCardsTable extends PersonCardsTable {
 				)
 				->where(['DataOwners.id IN' => $managerIds])
 				->toArray();
-			
-			$stack->set(['managers' => $dataOwner]);			
-		}		
+
+			$stack->set(['managers' => $dataOwner]);
+		}
 		return $stack;
 	}
-	
+
 }
