badd3d5272ccb4e56765c8f040ba3d5a70452aa1
dreamingmind <ddrake@dreamingmind.com>
2020-01-16 19:41:59 -0800

    Make new Prefs class house the constants

--- src/Lib/Prefs.php
+++ src/Lib/Prefs.php
@@ -7,4 +7,19 @@ namespace App\Lib;
 class Prefs extends PrefsBase
 {
 
+    const PAGINATION_LIMIT = 'pagination.limit';
+    const PAGINATION_SORT_PEOPLE = 'pagination.sort.people';
+    const PAGINATION_SORT_ARTWORK = 'pagination.sort.artwork';
+
+    public  $lists = [
+        Prefs::PAGINATION_SORT_PEOPLE => [
+            'values' => ['first_name', 'last_name'],
+            'select' => ['first_name' => 'First Name', 'last_name' => 'Last Name', 'x' => 'Bad Value']
+        ],
+        Prefs::PAGINATION_SORT_ARTWORK => [
+            'values' => [],
+            'select' => []
+        ],
+    ];
+
 }
--- src/Lib/PrefsBase.php
+++ src/Lib/PrefsBase.php
@@ -21,6 +21,28 @@ class PrefsBase
      */
     protected $form;
 
+    public  $lists = [];
+
+    /**
+     * Get the defined key => value list
+     * @param $path string
+     * @return array
+     */
+    public function selectList($path)
+    {
+        return $this->lists[$path]['select'];
+    }
+
+    /**
+     * Get the defined value list
+     *
+     * @param $path string
+     * @return array
+     */
+    public function values($path)
+    {
+        return $this->lists[$path]['values'];
+    }
 
 
     public function __construct(Preference $entity, PreferencesForm $form)
