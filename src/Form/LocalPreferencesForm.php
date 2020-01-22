6c96a7496f80da38cbc51468b3df318dda5bf159
dreamingmind <ddrake@dreamingmind.com>
2020-01-21 18:34:24 -0800

    remove old ‘constants’ class

--- src/Form/LocalPreferencesForm.php
+++ src/Form/LocalPreferencesForm.php
@@ -77,22 +77,3 @@ class LocalPreferencesForm extends PreferencesForm
     }
 
 }
-
-class PrefCon {
-
-//    const PAGINATION_LIMIT = 'pagination.limit';
-//    const PAGINATION_SORT_PEOPLE = 'pagination.sort.people';
-//    const PAGINATION_SORT_ARTWORK = 'pagination.sort.artwork';
-//
-//    static public  $listss = [
-//        PrefCon::PAGINATION_SORT_PEOPLE => [
-//            'values' => ['first_name', 'last_name'],
-//            'select' => ['first_name' => 'First Name', 'last_name' => 'Last Name', 'x' => 'Bad Value']
-//        ],
-//        PrefCon::PAGINATION_SORT_ARTWORK => [
-//            'values' => [],
-//            'select' => []
-//        ],
-//    ];
-
-}
