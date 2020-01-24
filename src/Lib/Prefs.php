c1ed85c5193bd5692e6f9f5e204bb400585512c6
dreamingmind <ddrake@dreamingmind.com>
2020-01-23 19:40:59 -0800

    remove accessors for removed property

--- src/Lib/Prefs.php
+++ src/Lib/Prefs.php
@@ -13,32 +13,10 @@ use http\Exception\BadQueryStringException;
 class Prefs extends PrefsBase
 {
 
-    public $formVariant = false;
 
-    public function __construct(Preference $entity, PreferencesForm $form, string $variant = null)
+    public function __construct(Preference $entity, PreferencesForm $form)
     {
         parent::__construct($entity, $form);
-        if (!is_null($variant)) {
-            $this->setFormVariant($variant);
-        }
-    }
-
-    public function setFormVariant($variant)
-    {
-        if (!in_array($variant, PrefCon::$variants)) {
-            $msg = 'Invalid preference variation requested';
-            throw new BadQueryStringException($msg);
-        }
-        $this->formVariant = $variant;
-    }
-
-    public function getFormVariant()
-    {
-        if ($this->formVariant == false) {
-            $msg = 'The specific variation of the preferences form was never specified';
-            throw new BadClassConfigurationException($msg);
-        }
-        return $this->formVariant;
     }
 
 }
