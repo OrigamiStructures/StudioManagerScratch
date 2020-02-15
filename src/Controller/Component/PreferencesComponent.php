b4759c7f940d8e206d15b7f310c8f26bfc3fc5af
dreamingmind <ddrake@dreamingmind.com>
2020-02-14 20:36:54 -0800

    shorten some lines with chaining

--- src/Controller/Component/PreferencesComponent.php
+++ src/Controller/Component/PreferencesComponent.php
@@ -181,16 +181,18 @@ class PreferencesComponent extends Component
         /* @var PreferencesForm $Form */
         /* @var PreferencesTable $PrefsTable */
 
+        /* @todo property is never useed */
+        /* @todo $user_id is never null */
         $this->user_id = $user_id;
         if (is_null($user_id)) {
             $UserPrefs = new Preference([]);
         } else {
-            $PrefsTable = TableRegistry::getTableLocator()->get('Preferences');
-            $UserPrefs = $PrefsTable->getPreferencesFor($user_id);
+            $UserPrefs = TableRegistry::getTableLocator()->get('Preferences')
+                ->getPreferencesFor($user_id);
         }
-        $Form = $this->getFormObjet();
 
-        $schema = $Form->schema();
+        /* @todo is there a lower impact way of getting schema than loading a Form object? */
+        $schema = $this->getFormObjet()->schema();
         $defaults = [];
         $prefs = [];
 
@@ -346,6 +348,9 @@ class PreferencesComponent extends Component
     public function getPrefs($user_id = null) : Prefs
     {
 
+        /*
+         * @todo kill registry. use property and separate, aliased components
+         */
         if (!isset($this->registry[$user_id])) {
             $entity = $this->getUserPrefsEntity($user_id);
             $this->registry[$user_id] = new Prefs(
