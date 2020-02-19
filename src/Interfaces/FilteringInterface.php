438f093dda88ef3032de722d5561140646a42fdb
dreamingmind <ddrake@dreamingmind.com>
2020-02-18 16:43:35 -0800

    comment interface

--- src/Interfaces/FilteringInterface.php
+++ src/Interfaces/FilteringInterface.php
@@ -9,6 +9,33 @@ use Cake\ORM\Query;
 interface FilteringInterface
 {
 
-    public function userFilter(Query $query);
+    /**
+     * User filters to work with paginated results
+     *
+     * Add user search to paginated results
+     *
+     * This method both prepares the values for the form that
+     * is displayed and applies current or save filter requests
+     * to the evoloving paginated query.
+     *
+     * The method must
+     *  - if there is post/put data
+     *      - add the conditions to $query
+     *      - put conditions in the session as required by IndexFilterManagementMiddleware
+     *  - else if there is a cached condition package
+     *      - add the conditions to $query
+     *  - set variables to support the search form
+     *  - return $query
+     *
+     * The provided query will be designed to get a seed set
+     * to pass to a stacksFor() request on a StacksTable
+     *
+     * Sessionized conditions have a scope of pages in which they will
+     * persist (as defined in the middleware).
+     *
+     * @param Query $query
+     * @return Query
+     */
+    public function userFilter(Query $query) : Query;
 
 }
